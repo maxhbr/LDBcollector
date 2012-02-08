594eceb608acd0703bdeba27a4ffd091f820de48
warp <kuno@frob.nl>
2012-02-08 06:10:57 -0500

    Add plaintext versions of the GNU and CC licenses to the repo.

---
+++ build/plaintext-cc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+DEST=$DIR/../plaintext
+
+# NOTE: most creative commons licenses do not have an official plain/text
+# version (http://creativecommons.org/weblog/entry/27094).  We're only
+# downloading the official plain/text versions.
+
+wget http://creativecommons.org/licenses/by/3.0/legalcode.txt --output-document $DEST/CC-BY-3.txt
+wget http://creativecommons.org/licenses/by-sa/3.0/legalcode.txt --output-document $DEST/CC-BY-SA-3.txt
+wget http://creativecommons.org/licenses/by-nd/3.0/legalcode.txt --output-document $DEST/CC-BY-ND-3.txt
+wget http://creativecommons.org/licenses/by-nc/3.0/legalcode.txt --output-document $DEST/CC-BY-NC-3.txt
+wget http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode.txt --output-document $DEST/CC-BY-NC-SA-3.txt
+wget http://creativecommons.org/licenses/by-nc-nd/3.0/legalcode.txt --output-document $DEST/CC-BY-NC-ND-3.txt
+wget http://creativecommons.org/publicdomain/zero/1.0/legalcode.txt --output-document $DEST/CC0-1.txt
---
+++ build/plaintext-gnu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+DEST=$DIR/../plaintext
+
+wget http://www.gnu.org/licenses/gpl-1.0.txt --output-document $DEST/GPL-1.txt
+wget http://www.gnu.org/licenses/gpl-2.0.txt --output-document $DEST/GPL-2.txt
+wget http://www.gnu.org/licenses/gpl-3.0.txt --output-document $DEST/GPL-3.txt
+wget http://www.gnu.org/licenses/agpl-3.0.txt --output-document $DEST/AGPL-3.txt
+wget http://www.gnu.org/licenses/lgpl-2.0.txt --output-document $DEST/LGPL-2.txt
+wget http://www.gnu.org/licenses/lgpl-2.1.txt --output-document $DEST/LGPL-2.1.txt
+wget http://www.gnu.org/licenses/lgpl-3.0.txt --output-document $DEST/LGPL-3.txt
+wget http://www.gnu.org/licenses/fdl-1.1.txt --output-document $DEST/GFDL-1.1.txt
+wget http://www.gnu.org/licenses/fdl-1.2.txt --output-document $DEST/GFDL-1.2.txt
+wget http://www.gnu.org/licenses/fdl-1.3.txt --output-document $DEST/GFDL-1.3.txt
