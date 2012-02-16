8cd1329832545450d3f911091d931a4ac08cf764
warp <kuno@frob.nl>
2012-02-16 17:17:09 -0500

    Add Affero 1 and 2.

---
+++ build/plaintext-affero.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+DEST=$DIR/../upstream/plaintext
+
+curl http://www.affero.org/source/latest.tar.gz | gunzip | tar xv ./svcs/doc/COPYING
+mv svcs/doc/COPYING $DEST/AGPL-1.txt
+
