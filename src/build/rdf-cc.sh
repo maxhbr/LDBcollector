c046b51efe3efcff7fce3d572eda360c091486c1
warp <kuno@frob.nl>
2012-11-25 21:42:36 +0100

    Update creative commons RDF files from git instead of subversion.

--- src/build/rdf-cc.sh
+++ src/build/rdf-cc.sh
@@ -4,31 +4,26 @@ SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-DEST=$DIR/../../upstream/rdf
+ROOT=$DIR/../..
+DEST=$ROOT/upstream/rdf
 
-mkdir --parents $DIR/.build
-cd $DIR/.build
-svn co http://code.creativecommons.org/svnroot/license.rdf/trunk/license_rdf/
+cd $ROOT
+## git submodule update --init
+mkdir --parents .build
 
-cd license_rdf
-rm creativecommons.org_licenses_GPL_2.0_.rdf
-rm creativecommons.org_licenses_LGPL_2.1_.rdf
-rm creativecommons.org_licenses_publicdomain_.rdf
-rm creativecommons.org_publicdomain_zero_1.0_.rdf
+ls upstream/creativecommons/cc/licenserdf/licenses/ > .build/filenames.txt
+cat .build/filenames.txt | sed 's/creativecommons.org_licenses_/CC-/' | sed 's/creativecommons.org_publicdomain_/CC-/' | sed 's/_/-/g' | sed 's/-by-/-BY-/' | sed 's/-nc-/-NC-/' | sed 's/-nd-/-ND-/' | sed 's/-sa-/-SA-/' | sed 's/sampling/Sampling/' | sed 's/devnations/Devnations/'  | sed 's/-.rdf/.rdf/' | sed 's/CC-mark-1.0/CC-PD-mark/' | sed 's/CC-zero-1.0/CC0/' > .build/newnames.txt
+cat .build/newnames.txt | sed "s,^,$DEST/," > .build/destnames.txt
+
+cd upstream/creativecommons/cc/licenserdf/licenses/
+paste $ROOT/.build/filenames.txt $ROOT/.build/destnames.txt | sed 's/^/cp /' | sh
+
+cd $ROOT/upstream/rdf
+
 rm Makefile.am
-
-ls > ../filenames.txt
-cat ../filenames.txt | sed 's/creativecommons.org_licenses_/CC-/' | sed 's/_/-/g' | sed 's/-by-/-BY-/' | sed 's/-nc-/-NC-/' | sed 's/-nd-/-ND-/' | sed 's/-sa-/-SA-/' | sed 's/sampling/Sampling/' | sed 's/devnations/Devnations/'  | sed 's/-.rdf/.rdf/' > ../newnames.txt
-
-paste ../filenames.txt ../newnames.txt | sed 's/^/mv /' | sh
-
-# for some reason the svn version is missing all dc:titles,
-# I hope this isn't true for all the other CC licenses as well.
-wget http://creativecommons.org/publicdomain/zero/1.0/rdf --output-document CC0.rdf
-
-# Another document where the online rdf version is different from the
-# version in subversion.
-wget http://creativecommons.org/licenses/by-nc-nd/2.0/jp/rdf --output-document CC-BY-NC-ND-2.0-jp.rdf
-
-mv *.rdf $DEST
+rm CC-BSD.rdf
+rm CC-GPL-2.0.rdf
+rm CC-LGPL-2.1.rdf
+rm CC-MIT.rdf
+rm CC-publicdomain.rdf
 
