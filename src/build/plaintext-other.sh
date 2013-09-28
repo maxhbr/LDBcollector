e0fe6dd7f95fd3429400229ed4fd12e3bfadd184
warp <kuno@frob.nl>
2013-09-28 13:40:53 +0200

    Add Repoze variant of BSD license.

--- src/build/plaintext-other.sh
+++ src/build/plaintext-other.sh
@@ -15,6 +15,7 @@ wget http://sam.zoy.org/wtfpl/COPYING --output-document $DEST/WTFPL-2.txt
 wget http://www.ncftp.com/ncftp/doc/LICENSE.txt --output-document $DEST/Clarified-Artistic.txt
 wget http://www.perlfoundation.org/attachment/legal/artistic-2_0.txt --output-document $DEST/Artistic-2.txt
 wget http://www.perlfoundation.org/attachment/legal/Artistic_1.0.txt --output-document $DEST/Artistic.txt
+wget http://repoze.org/LICENSE.txt --output-document $DEST/BSD-Repoze.txt
 
 # The IJG JPEG license doesn't seem to have a nice official plaintext
 # version.  Download an official release and extract the license text.
