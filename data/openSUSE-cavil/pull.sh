1a12d681d702e384c890f34d3235a6408a4e7f60
Maximilian Huber <gh@maxhbr.de>
2020-10-28 17:34:47 +0100

    add openSUSE-cavil pull script
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

---
+++ data/openSUSE-cavil/pull.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+tmpdir=$(mktemp -d)
+trap 'rm -rf $tmpdir' EXIT
+git clone https://github.com/openSUSE/cavil "$tmpdir"
+cp "$tmpdir/COPYING" ./
+cp -r "$tmpdir/lib/Cavil/resources/"* ./
