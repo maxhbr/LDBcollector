587c311929ae0c3d8cfde487c32b40048a9beb08
Martin Nonnenmacher <martin.nonnenmacher@bosch.io>
2022-01-18 22:01:15 +0100

    curation-generator: Add version ranges for the dotnot-runtime curations
    Generate version ranges for the dotnet-runtime curations, similar to how
    it was done for aspnetcore previously.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.io>

--- tools/curation-generator/build.gradle.kts
+++ tools/curation-generator/build.gradle.kts
@@ -55,17 +55,29 @@ tasks.register("generateDotNetRuntimeCurations") {
     group = "generate curations"
 
     doLast {
-        getFilesFromRepository(owner = "dotnet", repository = "runtime")
-            .filter { it.startsWith("src/libraries/") && it.endsWith(".sln") }
-            .forEach {
-                val project = it.substringAfterLast("/").removeSuffix(".sln")
-                val path = it.substringBeforeLast("/")
-                val id = Identifier("NuGet::$project")
+        val data = mutableListOf<PathCurationData>()
 
-                if ("System." in project || "Microsoft." in project) {
-                    createPathCuration(id, path)
-                }
+        getTagsFromRepository(owner = "dotnet", repository = "runtime")
+            .filter { it.isVersion() }
+            .forEach { tag ->
+                logger.quiet("Processing tag $tag.")
+
+                getFilesFromRepository(owner = "dotnet", repository = "runtime", ref = tag)
+                    .filter { it.startsWith("src/libraries/") && it.endsWith(".sln") }
+                    .forEach {
+                        val project = it.substringAfterLast("/").removeSuffix(".sln")
+                        val path = it.substringBeforeLast("/")
+                        val id = Identifier("NuGet::$project")
+
+                        if ("System." in project || "Microsoft." in project) {
+                            data += PathCurationData(id, path, tag)
+                        }
+                    }
             }
+
+        data.groupBy { it.id }.forEach { (_, curationData) ->
+            curationData.toPathCurations().forEach { saveCuration(it) }
+        }
     }
 }
 
