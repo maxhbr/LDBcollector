3604371f8ac401a64a6976007b91a6d0ea442e6b
Martin Delabre <delabre.martin@gmail.com>
2022-01-31 17:24:17 +0100

    Commenting a newly came Warning message #13
    Signed-off-by: Martin Delabre <delabre.martin@gmail.com>

--- hermine/hermine/settings.py
+++ hermine/hermine/settings.py
@@ -125,6 +125,7 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
+# The following line raises a Warning. Moving the folder to the right place does not fix it. 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 LOGIN_REDIRECT_URL = "/"
 
