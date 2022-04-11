a4b0c3b92c8d42e0d3df94b69014f4af267ab86a
David Cantrell <dcantrell@redhat.com>
2022-04-11 13:02:13 -0400

    Import not-approved licenses again
    The source spreadsheet for these was updated with more information and
    some deprecated licenses have been removed.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- original/tsv2toml-bad.py
+++ original/tsv2toml-bad.py
@@ -51,13 +51,13 @@ def write_licenses(outdir, licenses):
 
     for license in licenses.keys():
         entry = licenses[license]
-        output_file = os.path.join(outdir, license.strip().replace(" ", "_").replace("/", "-") + ".toml")
+        output_file = os.path.join(outdir, license.strip().replace(" ", "_").replace("/", "-").replace("(", "").replace(")", "") + ".toml")
 
         # write output file
         o = open(output_file, "w")
         o.write("[license]\n\n")
-        o.write("expression = [ \"%s\" ]\n" % entry["spdx_expression"].strip())
-        o.write("status = \"not approved\"\n")
+        o.write("expression = \"%s\"\n" % entry["spdx_expression"].strip())
+        o.write("status = [ \"not-approved\" ]\n")
 
         if entry["text"] != "":
             entry["text"] = entry["text"].strip()
