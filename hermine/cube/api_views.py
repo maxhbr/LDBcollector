77339517e33b046ff9a68976bf80448c95d2d3eb
Jill-Maud Royer <hello@jillroyer.me>
2022-04-27 16:08:39 +0200

    feat(api): get generic obligations from a sbom
    Signed-off-by: Jill-Maud Royer <hello@jillroyer.me>

--- hermine/cube/api_views.py
+++ hermine/cube/api_views.py
@@ -4,10 +4,11 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 from functools import reduce
+from itertools import groupby
 
 from django.db.models import Q
 from django_filters import rest_framework as filters, CharFilter
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -23,6 +24,7 @@ from cube.serializers import (
     UploadSPDXSerializer,
     DerogationSerializer,
     UploadORTSerializer,
+    SBOMSerializer,
 )
 from .importers import import_ort_evaluated_model_json_file, import_spdx_file
 from .models import (
@@ -40,6 +42,7 @@ from .utils.licenses import (
     get_licenses_to_check_or_create,
     get_usages_obligations,
     get_license_triggered_obligations,
+    get_licenses_triggered_obligations,
 )
 from .utils.releases import propagate_choices, update_validation_step
 
@@ -89,15 +92,10 @@ class GenericFilter(filters.FilterSet):
         modification = self.form.cleaned_data.get("modification")
 
         if exploitation or modification:
-            obligations_pk = set()
-            for license in licenses:
-                obligations_pk.update(
-                    o.pk
-                    for o in get_license_triggered_obligations(
-                        license, exploitation, modification
-                    )
-                )
-            queryset = queryset.filter(obligation__in=obligations_pk).distinct()
+            obligations = get_licenses_triggered_obligations(
+                licenses, exploitation, modification
+            )
+            queryset = queryset.filter(obligation__in=obligations).distinct()
 
         return queryset
 
@@ -112,6 +110,45 @@ class GenericViewSet(viewsets.ModelViewSet):
     lookup_field = "id"
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = GenericFilter
+
+    @action(detail=False, methods=["POST"])
+    def sbom(self, request):
+        """
+        Get list of generic obligations for a given SBOM.
+
+        Uploads a list of package with their licenses SPDX and return a list of generic obligations with
+        packages which triggered them.
+        """
+        serializer = SBOMSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        generics = (
+            {"generic": generic, "triggered_by": package["package_id"]}
+            for package in serializer.validated_data["packages"]
+            for generic in Generic.objects.filter(
+                obligation__in=get_license_triggered_obligations(
+                    License.objects.get(spdx_id=package["spdx"]),
+                    package["exploitation"],
+                    package["modification"],
+                )
+            ).distinct()
+        )
+
+        sort_key = lambda generic: generic["generic"].pk
+        sorted_generics = groupby(sorted(generics, key=sort_key), sort_key)
+        serializer_data = list()
+        for generic_pk, generics in sorted_generics:
+            generics = list(generics)
+            generic = generics[0]["generic"]
+            generic.triggered_by = [g["triggered_by"] for g in generics]
+            serializer_data.append(generic)
+
+        serializer = self.get_serializer(
+            serializer_data,
+            many=True,
+        )
+        return Response(serializer.data)
 
 
 class ProductViewSet(viewsets.ModelViewSet):
