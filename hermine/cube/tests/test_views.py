e480bcb014ea5042b436ebec2d5ed5f2583c2f9c
Jill-Maud Royer <hello@jillroyer.me>
2022-05-02 16:10:53 +0200

    tests: misc views smoke tests
    Signed-off-by: Jill-Maud Royer <hello@jillroyer.me>

--- hermine/cube/tests/test_views.py
+++ hermine/cube/tests/test_views.py
@@ -1,3 +1,18 @@
 # SPDX-FileCopyrightText: 2022 Martin Delabre <gitlab.com/delabre.martin>
 #
 # SPDX-License-Identifier: AGPL-3.0-only
+
+from django.test import TestCase
+from django.urls import reverse
+
+
+class MiscViewsTestCase(TestCase):
+    def test_home(self):
+        url = reverse("cube:root")
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_about(self):
+        url = reverse("cube:about")
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
