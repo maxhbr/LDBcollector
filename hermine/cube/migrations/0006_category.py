8c9a5d55169ee795fa4c5759df71f6c28229c90b
Jill-Maud Royer <hello@jillroyer.me>
2022-05-02 14:48:46 +0200

    chore: fix black formatting
    Signed-off-by: Jill-Maud Royer <hello@jillroyer.me>

--- hermine/cube/migrations/0006_category.py
+++ hermine/cube/migrations/0006_category.py
@@ -9,18 +9,34 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('cube', '0005_alter_release_product_alter_version_component'),
+        ("cube", "0005_alter_release_product_alter_version_component"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Category',
+            name="Category",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=200, unique=True)),
-                ('description', models.TextField(blank=True, max_length=500)),
-                ('owner', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
-                ('products', models.ManyToManyField(to='cube.product')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=200, unique=True)),
+                ("description", models.TextField(blank=True, max_length=500)),
+                (
+                    "owner",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.SET_NULL,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
+                ("products", models.ManyToManyField(to="cube.product")),
             ],
         ),
     ]
