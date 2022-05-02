b56a9bfc8276ef34cb55cd311f1cd69b1b13f65f
David Cantrell <dcantrell@redhat.com>
2022-05-02 15:51:14 -0400

    Python flake8 and black fixes for validate-spec.py
    Coding style and consistency.
    
    Signed-off-by: David Cantrell <dcantrell@redhat.com>

--- tools/validate-spec.py
+++ tools/validate-spec.py
@@ -40,11 +40,19 @@ import os
 import sys
 import toml
 
-status_values = ["approved", "approved-content", "approved-documentation", "approved-fonts", "not-approved"]
+status_values = [
+    "approved",
+    "approved-content",
+    "approved-documentation",
+    "approved-fonts",
+    "not-approved",
+]
+
 
 def usage(prog):
     print("Usage: %s [license data directory]" % prog)
     sys.exit(1)
+
 
 if __name__ == "__main__":
     r = 0
@@ -60,8 +68,10 @@ if __name__ == "__main__":
 
     for licensefile in os.scandir(datadir):
         # all license data files must be *.toml files
-        if not licensefile.name.endswith('.toml'):
-            sys.stderr.write("*** %s license file does not end with '.toml'\n" % licensefile.name)
+        if not licensefile.name.endswith(".toml"):
+            sys.stderr.write(
+                "*** %s license file does not end with '.toml'\n" % licensefile.name  # noqa: E501
+            )
             r = 1
             continue
 
@@ -73,42 +83,58 @@ if __name__ == "__main__":
         # all other keys are invalid at this level
         keys = [*data]
 
-        if 'license' not in keys:
-            sys.stderr.write("*** %s missing the [license] block\n" % licensefile.name)
+        if "license" not in keys:
+            sys.stderr.write("*** %s missing the [license] block\n" % licensefile.name)  # noqa: E501
             r = 1
         else:
-            keys.remove('license')
+            keys.remove("license")
 
-        if 'fedora' in keys:
+        if "fedora" in keys:
             # the fedora key is allowed, but not required
-            keys.remove('fedora')
+            keys.remove("fedora")
 
         if len(keys) != 0:
             for key in keys:
-                sys.stderr.write("*** %s contains invalid [%s] block\n" % licensefile.name)
+                sys.stderr.write(
+                    "*** %s contains invalid [%s] block\n" % licensefile.name
+                )
                 r = 1
 
         # check the license key
-        keys = [*data['license']]
+        keys = [*data["license"]]
 
-        if 'expression' not in keys or not isinstance(data['license']['expression'], str):
-            sys.stderr.write("*** %s missing the 'expression' key in the [license] block\n" % licensefile.name)
+        if "expression" not in keys or not isinstance(
+            data["license"]["expression"], str
+        ):
+            sys.stderr.write(
+                "*** %s missing the 'expression' key in the [license] block\n"
+                % licensefile.name
+            )
             r = 1
         else:
-            keys.remove('expression')
+            keys.remove("expression")
 
-        if 'status' not in keys:
-            sys.stderr.write("*** %s missing the 'status' key in the [license] block\n" % licensefile.name)
+        if "status" not in keys:
+            sys.stderr.write(
+                "*** %s missing the 'status' key in the [license] block\n"
+                % licensefile.name
+            )
             r = 1
         else:
-            status = data['license']['status']
+            status = data["license"]["status"]
 
             if isinstance(status, str) and status not in status_values:
-                sys.stderr.write("*** %s has invalid 'status' in the [license] block: %s\n" % (licensefile.name, status))
+                sys.stderr.write(
+                    "*** %s has invalid 'status' in the [license] block: %s\n"
+                    % (licensefile.name, status)
+                )
                 r = 1
             elif isinstance(status, list):
-                if 'not-approved' in status and len(status) > 1:
-                    sys.stderr.write("*** %s has 'not-approved' status combined with other status in the [license] block\n" % licensefile.name)
+                if "not-approved" in status and len(status) > 1:
+                    sys.stderr.write(
+                        "*** %s has 'not-approved' status combined with other status in the [license] block\n"  # noqa: E501
+                        % licensefile.name
+                    )
                     r = 1
 
                 for s in status_values:
@@ -116,57 +142,81 @@ if __name__ == "__main__":
                         status.remove(s)
 
                 if len(status) != 0:
-                    sys.stderr.write("*** %s has invalid status values in the [license] block: %s\n" % (licensefile.name, status))
+                    sys.stderr.write(
+                        "*** %s has invalid status values in the [license] block: %s\n"  # noqa: E501
+                        % (licensefile.name, status)
+                    )
                     r = 1
             else:
-                sys.stderr.write("*** %s has invalid 'status' in the [license] block\n" % licensefile.name)
+                sys.stderr.write(
+                    "*** %s has invalid 'status' in the [license] block\n"
+                    % licensefile.name
+                )
                 r = 1
 
-            keys.remove('status')
+            keys.remove("status")
 
-        if 'text' in keys:
-            if not isinstance(data['license']['text'], str):
-                sys.stderr.write("*** %s has 'text' value not of string type in the [license] block\n" % licensefile.name)
+        if "text" in keys:
+            if not isinstance(data["license"]["text"], str):
+                sys.stderr.write(
+                    "*** %s has 'text' value not of string type in the [license] block\n"  # noqa: E501
+                    % licensefile.name
+                )
                 r = 1
 
-            keys.remove('text')
+            keys.remove("text")
 
         if len(keys) != 0:
             for key in keys:
-                sys.stderr.write("*** %s contains invalid '%s' key in the [license] block\n" % (licensefile.name, key))
+                sys.stderr.write(
+                    "*** %s contains invalid '%s' key in the [license] block\n"
+                    % (licensefile.name, key)
+                )
                 r = 1
 
         # check the fedora key
-        keys = [*data['fedora']]
+        keys = [*data["fedora"]]
 
-        if 'name' in keys:
-            name = data['fedora']['name']
+        if "name" in keys:
+            name = data["fedora"]["name"]
 
             if not isinstance(name, list) and not isinstance(name, str):
-                sys.stderr.write("*** %s has 'name' value in the [fedora] block, but not a list or string\n" % licensefile.name)
+                sys.stderr.write(
+                    "*** %s has 'name' value in the [fedora] block, but not a list or string\n"  # noqa: E501
+                    % licensefile.name
+                )
                 r = 1
 
-            keys.remove('name')
+            keys.remove("name")
 
-        if 'abbreviation' in keys:
-            abbreviation = data['fedora']['abbreviation']
+        if "abbreviation" in keys:
+            abbreviation = data["fedora"]["abbreviation"]
 
-            if not isinstance(abbreviation, list) and not isinstance(abbreviation, str):
-                sys.stderr.write("*** %s has 'abbreviation' value in the [fedora] block, but not a list or string\n" % licensefile.name)
+            if not isinstance(abbreviation, list) and not isinstance(abbreviation, str):  # noqa: E501
+                sys.stderr.write(
+                    "*** %s has 'abbreviation' value in the [fedora] block, but not a list or string\n"  # noqa: E501
+                    % licensefile.name
+                )
                 r = 1
 
-            keys.remove('abbreviation')
+            keys.remove("abbreviation")
 
-        if 'notes' in keys:
-            if not isinstance(data['fedora']['notes'], str):
-                sys.stderr.write("*** %s has 'notes' value not of string type in the [fedora] block\n" % licensefile.name)
+        if "notes" in keys:
+            if not isinstance(data["fedora"]["notes"], str):
+                sys.stderr.write(
+                    "*** %s has 'notes' value not of string type in the [fedora] block\n"  # noqa: E501
+                    % licensefile.name
+                )
                 r = 1
 
-            keys.remove('notes')
+            keys.remove("notes")
 
         if len(keys) != 0:
             for key in keys:
-                sys.stderr.write("*** %s contains invalid '%s' key in the [fedora] block\n" % (licensefile.name, key))
+                sys.stderr.write(
+                    "*** %s contains invalid '%s' key in the [fedora] block\n"
+                    % (licensefile.name, key)
+                )
                 r = 1
 
     sys.exit(r)
