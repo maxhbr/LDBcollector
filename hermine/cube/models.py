ee3cd1c68787c4b04a666d49fc77bdfb858dc5cd
Jill-Maud Royer <hello@jillroyer.me>
2022-07-21 17:57:39 +0200

    feat: category field for product is optional
    Signed-off-by: Jill-Maud Royer <hello@jillroyer.me>

--- hermine/cube/models.py
+++ hermine/cube/models.py
@@ -35,7 +35,9 @@ class Product(models.Model):
     name = models.CharField(max_length=200, unique=True)
     description = models.TextField(max_length=500, blank=True)
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    categories = models.ManyToManyField("Category", db_table="cube_category_products")
+    categories = models.ManyToManyField(
+        "Category", db_table="cube_category_products", blank=True
+    )
 
     def __str__(self):
         return self.name
