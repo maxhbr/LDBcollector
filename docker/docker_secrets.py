406c1030a571778d4373adced6df3e0f2737b63b
Jill-Maud Royer <hello@jillroyer.me>
2022-09-12 17:31:27 +0200

    feat: oauth env params in docker django settings
    Signed-off-by: Jill-Maud Royer <hello@jillroyer.me>

--- docker/docker_secrets.py
+++ docker/docker_secrets.py
@@ -13,26 +13,32 @@ DATABASES = {
 
 SECRET_KEY = os.environ.get("SECRET")
 
-# # Load Auth0 application settings into memory
-AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN")
-AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID")
-AUTH0_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET")
-AUTH0_SCOPE = os.environ.get("AUTH0_SCOPE")         # list of scope separated by ','
-AUTH0_ID_KEY = os.environ.get("AUTH0_ID_KEY")
+# Load OAuth application settings into memory
+OAUTH_CLIENT_ID = os.environ.get("OAUTH_CLIENT_ID")
+OAUTH_DOMAIN = os.environ.get("OAUTH_DOMAIN")
+OAUTH_CLIENT_SECRET = os.environ.get("OAUTH_CLIENT_SECRET")
+OAUTH_SCOPE = os.environ.get("OAUTH_SCOPE")  # list of scope separated by ','
+OAUTH_ID_KEY = os.environ.get("OAUTH_ID_KEY")
+OAUTH_TOKEN_URL = os.environ.get("OAUTH_TOKEN_URL")
+OAUTH_AUTHORIZE_URL = os.environ.get("OAUTH_AUTHORIZE_URL")
+OAUTH_USER_URL = os.environ.get("OAUTH_USER_URL")
+OAUTH_USERNAME_PROPERTY = os.environ.get("OAUTH_USERNAME_PROPERTY", "username")
 
-# For configuring OAuthn the following parameters are required :
-if AUTH0_DOMAIN is not None:
+# For configuring OAuth the following parameters are required :
+if OAUTH_CLIENT_ID is not None:
     OAUTH_CLIENT = {
-        "client_id": AUTH0_CLIENT_ID,
-        "client_secret": AUTH0_CLIENT_SECRET,
+        "client_id": OAUTH_CLIENT_ID,
+        "client_secret": OAUTH_CLIENT_SECRET,
         "access_token_method": "POST",
-        "access_token_url": "https://{domain}/oauth/token".format(domain=AUTH0_DOMAIN),
-        "authorize_url": "https://{domain}/authorize".format(domain=AUTH0_DOMAIN),
-        "scope": AUTH0_SCOPE.split(","), # must be a array of scope, example ['openid', 'profile', 'email']
-        "user_url": "https://{domain}/userinfo".format(domain=AUTH0_DOMAIN), # provider URL for getting user data after token exchange
-        "id_key": AUTH0_ID_KEY,  # unique id of userinfo, example : sub
+        "access_token_url": OAUTH_TOKEN_URL,
+        "authorize_url": OAUTH_AUTHORIZE_URL,
+        "scope": OAUTH_SCOPE.split(
+            ","
+        ),  # must be a array of scope, example ['openid', 'profile', 'email']
+        "user_url": OAUTH_USER_URL,  # provider URL for getting user data after token exchange
+        "id_key": OAUTH_ID_KEY,  # unique id of userinfo, example : sub, or id
         "user_details": lambda res: {  # generates username and email from user data
-            "username": res.get("nickname"),
+            "username": res.get(OAUTH_USERNAME_PROPERTY),
             "email": res.get("email"),
         },
     }
