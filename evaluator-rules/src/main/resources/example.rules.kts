76fce2c8d919e2f25261dbe02e06b336b0eb19b6
Sebastian Schuberth <sschuberth@gmail.com>
2022-09-19 15:58:47 +0200

    example.rules.kts: Drop a superfluous case destinction
    Both code branches perform the same action.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- evaluator-rules/src/main/resources/example.rules.kts
+++ evaluator-rules/src/main/resources/example.rules.kts
@@ -1062,13 +1062,8 @@ fun RuleSet.copyleftLimitedInSourceRule() = packageRule("COPYLEFT_LIMITED_IN_SOU
 
         val licenseSourceName = licenseSource.name.lowercase()
         val message = if (licenseSource == LicenseSource.DETECTED) {
-            if (pkg.id.type == "Unmanaged") {
-                "The ScanCode copyleft-limited categorized license $license was $licenseSourceName in package " +
-                        "${pkg.id.toCoordinates()}."
-            } else {
-                "The ScanCode copyleft-limited categorized license $license was $licenseSourceName in package " +
-                        "${pkg.id.toCoordinates()}."
-            }
+            "The ScanCode copyleft-limited categorized license $license was $licenseSourceName in package " +
+                    "${pkg.id.toCoordinates()}."
         } else {
             "The package ${pkg.id.toCoordinates()} has the $licenseSourceName ScanCode copyleft-limited " +
                     "categorized license $license."
