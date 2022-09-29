60ac04e73efe6dffac30a8bc8c0857474e71c58c
Frank Viernau <frank_viernau@epam.com>
2022-09-29 09:36:08 +0200

    evaluator-rules: Fix-up a compilation error
    Compilation started to fail as of [1].
    
    [1] https://github.com/oss-review-toolkit/ort-config/commit/35faef661e70c4b81d130ce5afb3eb8de86c62e3
    
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

--- evaluator-rules/src/main/resources/example.rules.kts
+++ evaluator-rules/src/main/resources/example.rules.kts
@@ -1065,7 +1065,7 @@ fun RuleSet.copyleftLimitedInSourceRule() = packageRule("COPYLEFT_LIMITED_IN_SOU
             "The ScanCode copyleft-limited categorized license $license was $licenseSourceName in package " +
                     "${pkg.metadata.id.toCoordinates()}."
         } else {
-            "The package ${pkg.id.toCoordinates()} has the $licenseSourceName ScanCode copyleft-limited " +
+            "The package ${pkg.metadata.id.toCoordinates()} has the $licenseSourceName ScanCode copyleft-limited " +
                     "categorized license $license."
         }
 
