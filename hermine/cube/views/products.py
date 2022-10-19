ff4fb9c18d1386325116dd663f511365743bd2ee
Camille Moulin <cmoulin@inno3.fr>
2022-10-19 18:29:38 +0200

    fix: harmonize pagination and search to products
    Signed-off-by: Camille Moulin <cmoulin@inno3.fr>

--- hermine/cube/views/products.py
+++ hermine/cube/views/products.py
@@ -6,7 +6,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 from django.urls import reverse_lazy
+
 from cube.models import Product, Release
+from cube.views.mixins import SearchMixin
 
 
 class ProductListView(LoginRequiredMixin, generic.ListView):
@@ -32,10 +34,11 @@ class ProductAddView(LoginRequiredMixin, generic.CreateView):
     # success_url = reverse_lazy("cube:products")
 
 
-class ProductListView(LoginRequiredMixin, generic.ListView):
+class ProductListView(LoginRequiredMixin, SearchMixin, generic.ListView):
     model = Product
     template_name = "cube/product_list.html"
-    paginate_by = 20
+    search_fields = ("name", "description")
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
