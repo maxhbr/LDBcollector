3d04f84dcfd95ac9855f6d6e439bd7923b4b6e54
Maud Royer <hello@maudroyer.fr>
2023-01-10 17:11:47 +0100

    fix: import annotations in python 3.8
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

--- hermine/cube/utils/ort.py
+++ hermine/cube/utils/ort.py
@@ -1,6 +1,8 @@
 #  SPDX-FileCopyrightText: 2021 Hermine-team <hermine@inno3.fr>
 #
 #  SPDX-License-Identifier: AGPL-3.0-only
+from __future__ import annotations
+
 from dataclasses import dataclass
 from typing import Mapping, List
 
