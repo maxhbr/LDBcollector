71156b8361e583e00abc7da32ee53994214344df
Louis Marie <louis.marie@orange.com>
2023-01-19 10:30:35 +0000

    Update file oauth.py
    Signed-off-by: Louis Marie <louis.marie@orange.com>

--- hermine/cube/oauth.py
+++ hermine/cube/oauth.py
@@ -18,6 +18,7 @@ class OAuth2(BaseOAuth2):
     ACCESS_TOKEN_URL = settings.OAUTH_CLIENT["access_token_url"]
     ID_KEY = settings.OAUTH_CLIENT.get("id_key", "id")
     ACCESS_TOKEN_METHOD = settings.OAUTH_CLIENT.get("access_token_method", "POST")
+    REDIRECT_STATE = settings.OAUTH_CLIENT.get("redirect_state", True)
 
     # FIXME : override get_scope() OR redefine DEFAULT_SCOPE ?
     def get_scope(self):
