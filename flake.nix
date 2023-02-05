e1b4698fa60be100a824c3f868c12bc07c9d957e
Maximilian Huber <gh@maxhbr.de>
2023-02-05 11:36:37 +0100

    try to bootstrap rust
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- flake.nix
+++ flake.nix
@@ -1,11 +1,63 @@
+
+
 {
-  description = "A very basic flake";
+  description = "ramda.guide";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    naersk = {
+      url = "github:nmattia/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      flake = false;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+
+  outputs = { naersk, nixpkgs, rust-overlay, self, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        rust-overlay' = import rust-overlay;
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay' ];
+        };
+        rust = (pkgs.rustChannelOf {
+          date = "2022-10-29";
+          channel = "nightly";
+        }).rust;
+        naersk-lib = naersk.lib."${system}".override {
+          cargo = rust;
+          rustc = rust;
+        };
+      in rec {
+        packages = utils.lib.flattenTree {
+          ldbcolector = naersk-lib.buildPackage {
+            pname = "ldbcolector";
+            root = ./.;
+          };
+        };
+
+        defaultPackage = packages.ldbcolector;
+
+        apps.ldbcolector = utils.lib.mkApp {
+          drv = packages.ldbcolector;
+        };
+
+        defaultApp = apps.ldbcolector;
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs;[ 
+            rustup
+          ];
+          nativeBuildInputs = [ rust ];
+        };
+      }
+    );
 }
