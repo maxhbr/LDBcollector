8ced9fa85fb712f3bc78133a172188c5f983ffd2
Maximilian Huber <gh@maxhbr.de>
2023-02-15 22:27:43 +0100

    work on server
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- src/server.rs
+++ src/server.rs
@@ -7,33 +7,37 @@ use build_html::*;
 pub async fn serve(graph: Box<LicenseGraph>) {
     let graph2 = graph.clone();
 
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let ldb = warp::path!("license" / String)
-        .map(move |license: String| {
-            let focused = graph.focus(crate::model::core::LicenseName::new(license.to_string().clone()));
-            format!("<body>\n<pre>\n{:?}\n</pre>\n</body>", focused.get_as_dot())
-        });
-
     // GET / -> index html
     let index = warp::path::end().map(move ||{
         let license_names = graph2.get_license_names();
-        let index: String = HtmlPage::new()
+        let html: String = HtmlPage::new()
             .with_title("ldbcollector")
             .with_header(1, "Licenses:")
             .with_container(
                 license_names.iter()
                     .fold( Container::new(ContainerType::Div), |acc, license_name| {
-                        acc.with_paragraph(format!("{:?}", license_name))
+                        acc.with_container(
+                            Container::new(ContainerType::Div)
+                                .with_link(format!("license/{}", license_name), format!("{:?}", license_name))
+                        )
                     })
-                // Container::new(ContainerType::Article)
-                //     .with_attributes([("id", "article1")])
-                //     .with_header_attr(2, "Hello, World", [("id", "article-head")])
-                //     .with_paragraph("This is a simple HTML demo")
             )
             .to_html_string();
 
-        warp::reply::html(index)
+        warp::reply::html(html)
     });
+
+    // GET /license/MIT
+    let ldb = warp::path!("license" / String)
+        .map(move |license: String| {
+            let focused = graph.focus(crate::model::core::LicenseName::new(license.to_string().clone()));
+            let html: String = HtmlPage::new()
+                .with_title("license")
+                .with_header(1, "dot:")
+                .with_preformatted(format!("{}", focused.get_as_dot()))
+                .to_html_string();
+            warp::reply::html(html)
+        });
 
     warp::serve(index.or(ldb))
         .run(([127, 0, 0, 1], 3030))
@@ -47,15 +51,3 @@ pub fn sync_serve(graph: Box<LicenseGraph>) {
         .unwrap()
         .block_on(server::serve(graph))
 }
-
-
-static INDEX_HTML: &str = r#"<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>ldbcollector</title>
-    </head>
-    <body>
-        <h1>ldbcollector</h1>
-    </body>
-</html>
-"#;
