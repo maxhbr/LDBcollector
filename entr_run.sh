de6f24e877b17566b01f9614a37fc8efb50c4bea
Maximilian Huber <gh@maxhbr.de>
2023-02-19 12:50:22 +0100

    wip
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- entr_run.sh
+++ entr_run.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+git submodule init
+git submodule update
 
 while sleep 1; do
   find src -iname '*.rs' | entr -dr cargo run;
