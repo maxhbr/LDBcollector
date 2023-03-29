d177f9a2e344e39fc48b561a8c8f1803542141f8
Maud Royer <hello@maudroyer.fr>
2023-03-29 15:12:36 +0200

    feat: create usage form + autocomplete
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

---
+++ hermine/cube/forms/widgets.py
@@ -0,0 +1,27 @@
+#  SPDX-FileCopyrightText: 2021 Hermine-team <hermine@inno3.fr>
+#
+#  SPDX-License-Identifier: AGPL-3.0-only
+
+from django import forms
+from django.contrib.admin.widgets import AutocompleteMixin, get_select2_language
+from django.urls import reverse
+
+
+class AutocompleteWidget(AutocompleteMixin, forms.Select):
+    def __init__(self, field, attrs=None, choices=(), using=None):
+        self.field = field
+        self.db = using
+        self.choices = choices
+        self.attrs = {} if attrs is None else attrs.copy()
+        self.i18n_name = get_select2_language()
+
+    def get_url(self):
+        return reverse("admin:autocomplete")
+
+    @property
+    def media(self):
+        return super().media + forms.Media(
+            css={
+                "screen": ("cube/css/autocomplete.css",),
+            },
+        )
