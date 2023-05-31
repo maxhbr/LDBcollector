4ae57b2658f502d4e51b2091b2e13fa950dd005b
Maud Royer <hello@maudroyer.fr>
2023-05-31 16:42:18 +0200

    fix: group creation on first migrate
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

--- hermine/cube/tests/test_models.py
+++ hermine/cube/tests/test_models.py
@@ -2,6 +2,7 @@
 # SPDX-FileCopyrightText: 2022 Martin Delabre <gitlab.com/delabre.martin>
 #
 # SPDX-License-Identifier: AGPL-3.0-only
+from django.contrib.auth.models import Group
 from django.test import TestCase
 
 from cube.models import (
@@ -34,3 +35,9 @@ class ComponentTestCase(TestCase):
         )
         component.refresh_from_db()
         self.assertEqual(component.package_repo, "npm")
+
+
+class DefaultPermissionTestCase(TestCase):
+    def default_groups_have_permissions(self):
+        pm = Group.objects.get(name="Project manager (install preset)")
+        self.assertTrue(pm.permissions.filter(codename="view_product").exists())
