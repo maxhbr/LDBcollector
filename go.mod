1a1a70b4a8064eda1afe648039f14cc51eb6a755
Kavya Shukla <kavyuushukla59@gmail.com>
2023-06-01 04:08:47 +0530

    chore: added project structure and initial files.
    Signed-off-by: Kavya Shukla <kavyuushukla59@gmail.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fossology/LicenseDb
+
+go 1.18
---
+++ pkg/models/doc.go
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: 2023 Kavya Shukla <kavyuushukla@gmail.com>
+// SPDX-License-Identifier: GPL-2.0
+
+// Package models is made to connect the database and to store the structs of 
+//different data.
+package models
