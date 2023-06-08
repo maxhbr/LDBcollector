64659f61f6ade38a10fff453837921917cb2cca3
Kavya Shukla <kavyuushukla59@gmail.com>
2023-06-08 16:16:56 +0530

    feat: add populating of database from json file
    Signed-off-by: Kavya Shukla <kavyuushukla59@gmail.com>

---
+++ pkg/models/types.go
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: 2023 Kavya Shukla <kavyuushukla@gmail.com>
+// SPDX-License-Identifier: GPL-2.0-only
+
+package models
+
+//
+type License struct {
+	Shortname       string `json:"rf_shortname" gorm:"primary_key"`
+	Fullname        string `json:"rf_fullname"`
+	Text            string `json:"rf_text"`
+	Url             string `json:"rf_url"`
+	AddDate         string `json:"rf_add_date"`
+	Copyleft        string `json:"rf_copyleft"`
+	FSFfree         string `json:"rf_FSFfree"`
+	OSIapproved     string `json:"rf_OSIapproved"`
+	GPLv2compatible string `json:"rf_GPLv2compatible"`
+	GPLv3compatible string `json:"rf_GPLv3compatible"`
+	Notes           string `json:"rf_notes"`
+	Fedora          string `json:"rf_Fedora"`
+	TextUpdatable   string `json:"rf_text_updatable"`
+	DetectorType    string `json:"rf_detector_type"`
+	Active          string `json:"rf_active"`
+	Source          string `json:"rf_source"`
+	SpdxCompatible  string `json:"rf_spdx_id"`
+	Risk            string `json:"rf_risk"`
+	Flag            string `json:"rf_flag"`
+	Marydone        string `json:"marydone"`
+}
