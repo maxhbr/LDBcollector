avinal/LicenseDb

feat: added two api endpoints i.e, create and update

pkg/api/api.go
@@ -15,6 +15,17 @@ import (
 
 var DB *gorm.DB
 
+func HandleInvalidUrl(c *gin.Context) {
+
+	er := models.LicenseError{
+		Status:    http.StatusNotFound,
+		Message:   "No such path exists please check URL",
+		Error:     "invalid path",
+		Path:      c.Request.URL.Path,
+		Timestamp: time.Now().Format(time.RFC3339),
+	}
+	c.JSON(http.StatusNotFound, er)
+}
 func GetAllLicense(c *gin.Context) {
 	var licenses []models.License
 
@@ -24,7 +35,7 @@ func GetAllLicense(c *gin.Context) {
 			Status:    http.StatusBadRequest,
 			Message:   "Licenses not found",
 			Error:     err.Error(),
-			Path:      "/api/licenses",
+			Path:      c.Request.URL.Path,
 			Timestamp: time.Now().Format(time.RFC3339),
 		}
 		c.JSON(http.StatusBadRequest, er)
@@ -33,6 +44,9 @@ func GetAllLicense(c *gin.Context) {
 	res := models.LicenseResponse{
 		Data:   licenses,
 		Status: http.StatusOK,
+		Meta: models.Meta{
+			ResourceCount: len(licenses),
+		},
 	}
 
 	c.JSON(http.StatusOK, res)
@@ -53,7 +67,7 @@ func GetLicense(c *gin.Context) {
 			Status:    http.StatusBadRequest,
 			Message:   fmt.Sprintf("no license with shortname '%s' exists", queryParam),
 			Error:     err.Error(),
-			Path:      fmt.Sprintf("/api/license/%s", queryParam),
+			Path:      c.Request.URL.Path,
 			Timestamp: time.Now().Format(time.RFC3339),
 		}
 		c.JSON(http.StatusBadRequest, er)
@@ -63,7 +77,85 @@ func GetLicense(c *gin.Context) {
 	res := models.LicenseResponse{
 		Data:   []models.License{license},
 		Status: http.StatusOK,
+		Meta: models.Meta{
+			ResourceCount: 1,
+		},
 	}
 
 	c.JSON(http.StatusOK, res)
+}
+
+func CreateLicense(c *gin.Context) {
+	var input models.LicenseInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		er := models.LicenseError{
+			Status:    http.StatusBadRequest,
+			Message:   fmt.Sprintf("invalid request"),
+			Error:     err.Error(),
+			Path:      c.Request.URL.Path,
+			Timestamp: time.Now().Format(time.RFC3339),
+		}
+		c.JSON(http.StatusBadRequest, er)
+		return
+	}
+
+	if input.Active == "" {
+		input.Active = "t"
+	}
+
+	license := models.License(input)
+
+	DB.Create(&license)
+
+	c.JSON(http.StatusOK, gin.H{"data": license})
+}
+
+func UpdateLicense(c *gin.Context) {
+	var update models.License
+	var license models.License
+	shortname := c.Param("shortname")
+	if err := DB.First(&license, shortname).Error; err != nil {
+		er := models.LicenseError{
+			Status:    http.StatusBadRequest,
+			Message:   fmt.Sprintf("license not found"),
+			Error:     err.Error(),
+			Path:      c.Request.URL.Path,
+			Timestamp: time.Now().Format(time.RFC3339),
+		}
+		c.JSON(http.StatusBadRequest, er)
+		return
+	}
+	if err := c.ShouldBindJSON(&update); err != nil {
+		er := models.LicenseError{
+			Status:    http.StatusBadRequest,
+			Message:   fmt.Sprintf("invalid request"),
+			Error:     err.Error(),
+			Path:      c.Request.URL.Path,
+			Timestamp: time.Now().Format(time.RFC3339),
+		}
+		c.JSON(http.StatusBadRequest, er)
+		return
+	}
+	if err := DB.Model(&license).Updates(update).Error; err != nil {
+		er := models.LicenseError{
+			Status:    http.StatusBadRequest,
+			Message:   fmt.Sprintf("Failed to update license"),
+			Error:     err.Error(),
+			Path:      c.Request.URL.Path,
+			Timestamp: time.Now().Format(time.RFC3339),
+		}
+		c.JSON(http.StatusInternalServerError, er)
+		return
+	}
+	res := models.LicenseResponse{
+		Data:   []models.License{license},
+		Status: http.StatusOK,
+		Meta: models.Meta{
+			ResourceCount: 1,
+		},
+	}
+
+	c.JSON(http.StatusOK, res)
+
 }

maxhbr/LDBcollector

    Create License:
        to create an open-source license and add it to the database
        to make a license we need some required field
    Update License:
        to update an open-source license and modify it to database
        License to be updated using shortname

pkg/api/api.go
@@ -15,8 +15,19 @@ import (
 
 var DB *gorm.DB
 
+func HandleInvalidUrl(c *gin.Context) {
+
+	er := models.LicenseError{
+		Status:    http.StatusNotFound,
+		Message:   "No such path exists please check URL",
+		Error:     "invalid path",
+		Path:      c.Request.URL.Path,
+		Timestamp: time.Now().Format(time.RFC3339),
+	}
+	c.JSON(http.StatusNotFound, er)
+}
 func GetAllLicense(c *gin.Context) {
-	var licenses []models.License
+	var licenses []models.LicenseDB
 
 	err := DB.Find(&licenses).Error
 	if err != nil {
@@ -24,7 +35,7 @@ func GetAllLicense(c *gin.Context) {
 			Status:    http.StatusBadRequest,
 			Message:   "Licenses not found",
 			Error:     err.Error(),
-			Path:      "/api/licenses",
+			Path:      c.Request.URL.Path,
 			Timestamp: time.Now().Format(time.RFC3339),
 		}
 		c.JSON(http.StatusBadRequest, er)
@@ -33,13 +44,16 @@ func GetAllLicense(c *gin.Context) {
 	res := models.LicenseResponse{
 		Data:   licenses,
 		Status: http.StatusOK,
+		Meta: models.Meta{
+			ResourceCount: len(licenses),
+		},
 	}
 
 	c.JSON(http.StatusOK, res)
 }
 
 func GetLicense(c *gin.Context) {
-	var license models.License
+	var license models.LicenseDB
 
 	queryParam := c.Param("shortname")
 	if queryParam == "" {
@@ -53,7 +67,7 @@ func GetLicense(c *gin.Context) {
 			Status:    http.StatusBadRequest,
 			Message:   fmt.Sprintf("no license with shortname '%s' exists", queryParam),
 			Error:     err.Error(),
-			Path:      fmt.Sprintf("/api/license/%s", queryParam),
+			Path:      c.Request.URL.Path,
 			Timestamp: time.Now().Format(time.RFC3339),
 		}
 		c.JSON(http.StatusBadRequest, er)
@@ -61,9 +75,116 @@ func GetLicense(c *gin.Context) {
 	}
 
 	res := models.LicenseResponse{
-		Data:   []models.License{license},
+		Data:   []models.LicenseDB{license},
 		Status: http.StatusOK,
+		Meta: models.Meta{
+			ResourceCount: 1,
+		},
 	}
 
 	c.JSON(http.StatusOK, res)
+}
+
+func CreateLicense(c *gin.Context) {
+	var input models.LicenseInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		er := models.LicenseError{
+			Status:    http.StatusBadRequest,
+			Message:   "invalid json body",
+			Error:     err.Error(),
+			Path:      c.Request.URL.Path,
+			Timestamp: time.Now().Format(time.RFC3339),
+		}
+		c.JSON(http.StatusBadRequest, er)
+		return
+	}
+
+	if input.Active == "" {
+		input.Active = "t"
+	}
+	license := models.LicenseDB(input)
+
+	result := DB.FirstOrCreate(&license)
+	if result.RowsAffected == 0 {
+
+		er := models.LicenseError{
+			Status:    http.StatusBadRequest,
+			Message:   "can not create license with same shortname",
+			Error:     fmt.Sprintf("Error: License with shortname '%s' already exists", input.Shortname),
+			Path:      c.Request.URL.Path,
+			Timestamp: time.Now().Format(time.RFC3339),
+		}
+		c.JSON(http.StatusBadRequest, er)
+		return
+	}
+	if result.Error != nil {
+		er := models.LicenseError{
+			Status:    http.StatusInternalServerError,
+			Message:   "Failed to create license",
+			Error:     result.Error.Error(),
+			Path:      c.Request.URL.Path,
+			Timestamp: time.Now().Format(time.RFC3339),
+		}
+		c.JSON(http.StatusInternalServerError, er)
+		return
+	}
+	res := models.LicenseResponse{
+		Data:   []models.LicenseDB{license},
+		Status: http.StatusCreated,
+		Meta: models.Meta{
+			ResourceCount: 1,
+		},
+	}
+
+	c.JSON(http.StatusCreated, res)
+}
+
+func UpdateLicense(c *gin.Context) {
+	var update models.LicenseDB
+	var license models.LicenseDB
+	shortname := c.Param("shortname")
+	if err := DB.Where("shortname = ?", shortname).First(&license).Error; err != nil {
+		er := models.LicenseError{
+			Status:    http.StatusBadRequest,
+			Message:   fmt.Sprintf("license with shortname '%s' not found", shortname),
+			Error:     err.Error(),
+			Path:      c.Request.URL.Path,
+			Timestamp: time.Now().Format(time.RFC3339),
+		}
+		c.JSON(http.StatusBadRequest, er)
+		return
+	}
+	if err := c.ShouldBindJSON(&update); err != nil {
+		er := models.LicenseError{
+			Status:    http.StatusBadRequest,
+			Message:   "invalid json body",
+			Error:     err.Error(),
+			Path:      c.Request.URL.Path,
+			Timestamp: time.Now().Format(time.RFC3339),
+		}
+		c.JSON(http.StatusBadRequest, er)
+		return
+	}
+	if err := DB.Model(&license).Updates(update).Error; err != nil {
+		er := models.LicenseError{
+			Status:    http.StatusInternalServerError,
+			Message:   "Failed to update license",
+			Error:     err.Error(),
+			Path:      c.Request.URL.Path,
+			Timestamp: time.Now().Format(time.RFC3339),
+		}
+		c.JSON(http.StatusInternalServerError, er)
+		return
+	}
+	res := models.LicenseResponse{
+		Data:   []models.LicenseDB{license},
+		Status: http.StatusOK,
+		Meta: models.Meta{
+			ResourceCount: 1,
+		},
+	}
+
+	c.JSON(http.StatusOK, res)
+
 }
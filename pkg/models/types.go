554165e9736da8eedf9efe8ae352a17a00b9f193
Kavya Shukla <kavyuushukla59@gmail.com>
2023-06-22 13:31:51 +0530

    feat: added two api endpoints i.e, create and update
        Create License:
            to create an open-source license and add it to the database
            to make a license we need some required field
        Update License:
            to update an open-source license and modify it to database
            License to be updated using shortname

--- pkg/models/types.go
+++ pkg/models/types.go
@@ -3,7 +3,10 @@
 
 package models
 
-type License struct {
+// The License struct represents a license entity with various attributes and
+// properties associated with it.
+// It provides structured storage for license-related information.
+type LicenseDB struct {
 	Shortname       string `json:"rf_shortname" gorm:"primary_key"`
 	Fullname        string `json:"rf_fullname"`
 	Text            string `json:"rf_text"`
@@ -26,22 +29,81 @@ type License struct {
 	Marydone        string `json:"marydone"`
 }
 
+type LicenseJson struct {
+	Shortname       string `json:"rf_shortname"`
+	Fullname        string `json:"rf_fullname"`
+	Text            string `json:"rf_text"`
+	Url             string `json:"rf_url"`
+	AddDate         string `json:"rf_add_date"`
+	Copyleft        string `json:"rf_copyleft"`
+	FSFfree         string `json:"rf_FSFfree"`
+	OSIapproved     string `json:"rf_OSIapproved"`
+	GPLv2compatible string `json:"rf_GPLv2compatible"`
+	GPLv3compatible string `json:"rf_GPLv3compatible"`
+	Notes           string `json:"rf_notes"`
+	Fedora          string `json:"rf_Fedora"`
+	TextUpdatable   string `json:"rf_text_updatable"`
+	DetectorType    string `json:"rf_detector_type"`
+	Active          string `json:"rf_active"`
+	Source          string `json:"rf_source"`
+	SpdxCompatible  string `json:"rf_spdx_compatible"`
+	Risk            string `json:"rf_risk"`
+	Flag            string `json:"rf_flag"`
+	Marydone        string `json:"marydone"`
+}
+
+// The Meta struct represents additional metadata associated with a
+// license retrieval operation.
+// It contains information that provides context and supplementary details
+// about the retrieved license data.
 type Meta struct {
 	ResourceCount int `json:"resource_count"`
 	Page          int `json:"page,omitempty"`
 	PerPage       int `json:"per_page,omitempty"`
 }
 
+// LicenseResponse struct is representation of design API response of license.
+// The LicenseResponse struct represents the response data structure for
+// retrieving license information.
+// It is used to encapsulate license-related data in an organized manner.
 type LicenseResponse struct {
-	Status int       `json:"status"`
-	Data   []License `json:"data"`
-	Meta   Meta      `json:"meta,omitempty"`
+	Status int         `json:"status"`
+	Data   []LicenseDB `json:"data"`
+	Meta   Meta        `json:"meta"`
 }
 
+// The LicenseError struct represents an error response related to license operations.
+// It provides information about the encountered error, including details such as
+// status, error message, error type, path, and timestamp.
 type LicenseError struct {
 	Status    int    `json:"status"`
 	Message   string `json:"message"`
 	Error     string `json:"error"`
 	Path      string `json:"path"`
 	Timestamp string `json:"timestamp"`
+}
+
+// The LicenseInput struct represents the input or payload required for creating a license.
+// It contains various fields that capture the necessary information for defining a license entity.
+type LicenseInput struct {
+	Shortname       string `json:"rf_shortname" binding:"required"`
+	Fullname        string `json:"rf_fullname" binding:"required"`
+	Text            string `json:"rf_text" binding:"required"`
+	Url             string `json:"rf_url" binding:"required"`
+	AddDate         string `json:"rf_add_date"`
+	Copyleft        string `json:"rf_copyleft"`
+	FSFfree         string `json:"rf_FSFfree"`
+	OSIapproved     string `json:"rf_OSIapproved"`
+	GPLv2compatible string `json:"rf_GPLv2compatible"`
+	GPLv3compatible string `json:"rf_GPLv3compatible"`
+	Notes           string `json:"rf_notes"`
+	Fedora          string `json:"rf_Fedora"`
+	TextUpdatable   string `json:"rf_text_updatable"`
+	DetectorType    string `json:"rf_detector_type"`
+	Active          string `json:"rf_active"`
+	Source          string `json:"rf_source"`
+	SpdxId          string `json:"rf_spdx_id" binding:"required"`
+	Risk            string `json:"rf_risk"`
+	Flag            string `json:"rf_flag"`
+	Marydone        string `json:"marydone"`
 }
---
+++ pkg/utils/util.go
@@ -0,0 +1,37 @@
+// SPDX-FileCopyrightText: 2023 Kavya Shukla <kavyuushukla@gmail.com>
+// SPDX-License-Identifier: GPL-2.0-only
+
+package utils
+
+import "github.com/fossology/LicenseDb/pkg/models"
+
+func Converter(input models.LicenseJson) models.LicenseDB {
+	if input.SpdxCompatible == "t" {
+		input.SpdxCompatible = input.Shortname
+	} else {
+		input.SpdxCompatible = "LicenseRef-fossology-" + input.Shortname
+	}
+	result := models.LicenseDB{
+		Shortname:       input.Shortname,
+		Fullname:        input.Fullname,
+		Text:            input.Text,
+		Url:             input.Fullname,
+		Copyleft:        input.Copyleft,
+		AddDate:         input.AddDate,
+		FSFfree:         input.FSFfree,
+		OSIapproved:     input.OSIapproved,
+		GPLv2compatible: input.GPLv2compatible,
+		GPLv3compatible: input.GPLv3compatible,
+		Notes:           input.Notes,
+		Fedora:          input.Fedora,
+		TextUpdatable:   input.TextUpdatable,
+		DetectorType:    input.DetectorType,
+		Active:          input.Active,
+		Source:          input.Source,
+		SpdxId:          input.SpdxCompatible,
+		Risk:            input.Risk,
+		Flag:            input.Flag,
+		Marydone:        input.Marydone,
+	}
+	return result
+}
