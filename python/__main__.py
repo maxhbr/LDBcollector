d4693c6358c6e71e5afe662ed3d1e59a81322218
Henrik Sandlef <hesa@sandklef.com>
2023-08-10 09:23:46 +0200

    Use new API

--- python/__main__.py
+++ python/__main__.py
@@ -14,10 +14,16 @@ print(f'l: {l}')
 l = ldb.licenses()
 print(f'all  size: {len(l)}')
 
-l = ldb.licenses(["Henrik Sandklef <hesa@sandklef.com>"])
+l = ldb.licenses(orgs=["Sandklef FOSS Labs AB"])
 print(f'hesa size: {len(l)}')
 
-l = ldb.licenses(["Someone else"])
+l = ldb.licenses(orgs=["Sandklef GNU Labs"])
+print(f'gnu size: {len(l)}')
+
+l = ldb.licenses(orgs=["Sandklef GNU Labs", "Sandklef FOSS Labs AB"])
+print(f'sandklef size: {len(l)}')
+
+l = ldb.licenses(orgs=["Someone else"])
 print(f'else size: {len(l)}')
 
 
