ec4b48e76ed34bff5b24409afe7bea1e6662d88b
Sandklef Henrik <hesa@sandklef.com>
2023-08-14 09:33:40 +0200

    sample main

--- python/__main__.py
+++ python/__main__.py
@@ -4,26 +4,17 @@ import logging
 
 from license_db import LicenseDatabase
 
-
 logging.basicConfig(level=logging.DEBUG)
 ldb = LicenseDatabase(check=True)
-
-l = ldb.license_info_spdxid("GPL-2.0-or-later")
-print(f'l: {l}')
 
 l = ldb.licenses()
 print(f'all  size: {len(l)}')
 
-l = ldb.licenses(orgs=["Sandklef FOSS Labs AB"])
-print(f'hesa size: {len(l)}')
+l = ldb.license("GPLv2+")
+print(f'l: {l}')
 
-l = ldb.licenses(orgs=["Sandklef GNU Labs"])
-print(f'gnu size: {len(l)}')
+l = ldb.license_scancode_key("GPLv2+")
+print(f'l: {l}')
 
-l = ldb.licenses(orgs=["Sandklef GNU Labs", "Sandklef FOSS Labs AB"])
-print(f'sandklef size: {len(l)}')
-
-l = ldb.licenses(orgs=["Someone else"])
-print(f'else size: {len(l)}')
-
-
+l = ldb.license_spdxid("GPLv2+")
+print(f'l: {l}')
