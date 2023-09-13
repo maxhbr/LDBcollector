df65e34e405040998d8e1b5e7e537fe69a25cc4c
Maud Royer <hello@maudroyer.fr>
2023-09-13 15:29:57 +0200

    fix: handling of ExpressionError in license_expression lib
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

--- hermine/cube/utils/validators.py
+++ hermine/cube/utils/validators.py
@@ -3,11 +3,20 @@
 #  SPDX-License-Identifier: AGPL-3.0-only
 
 from django.core.exceptions import ValidationError
+from license_expression import ExpressionError
+
 from .spdx import licensing, has_ors
 
 
 def validate_spdx_expression(spdx_expression: str):
-    info = licensing.validate(spdx_expression)
+    try:
+        # We have to do that before validate because some ExpressionError are not
+        # correctly handled by licensing.validate
+        licensing.parse(spdx_expression, strict=True)
+    except ExpressionError as e:
+        raise ValidationError(f"{spdx_expression} is not a valid SPDX expression : {e}")
+
+    info = licensing.validate(spdx_expression, strict=True)
     if len(info.errors) > 0:
         raise ValidationError(
             f"{spdx_expression} is not a valid SPDX expression : {', '.join(info.errors)}"
