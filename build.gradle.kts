d6d110ede23932aa53eff47d55cc8b7db2a4b649
Sebastian Schuberth <sebastian@doubleopen.org>
2023-11-15 23:27:12 +0100

    build: Add a minimum Gradle framework for editing ORT scripts
    In order to get auto-completion for custom ORT scripts working, the
    scripting definitions need to be known. To make them known, a Gradle
    project which pulls in the respective dependencies is required. Add the
    required Gradle files so that opening a Gradle build file in IntelliJ or
    opening the folder in Fleet [1] will load the scripting definitions, and
    subsequently opening *.rules.kts, *.notifications.kts, or
    *.how-to-fix-text-provider.kts files will make auto-completion work.
    
    [1]: https://www.jetbrains.com/fleet/
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.bundles.ortScriptDefinitions)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ORT Scripting Environment"
