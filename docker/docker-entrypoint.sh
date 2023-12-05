dc6b257cd6873489b727e9426c268b52a62976d0
François Barbe <f.barbe@lectra.com>
2023-12-05 15:53:16 +0100

    improve gunicorn conf

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -15,7 +15,7 @@ fi
 
 if test -n "$SUPERUSER" && test -n "$PASSWORD"
 then
-  f_log "create superuser if none exists with $USER"
+  f_log "create superuser if none exists with $SUPERUSER"
   python ./manage.py createsuperuser_if_none_exists --user="$SUPERUSER" --password="$PASSWORD"
 fi
-python ./manage.py runserver 0.0.0.0:"$DJANGO_PORT"
+python -m gunicorn --workers=$NB_WORKERS --threads=$NB_THREADS --worker-class=gthread --worker-tmp-dir /dev/shm hermine.wsgi -b 0.0.0.0:"$DJANGO_PORT" -t 120
