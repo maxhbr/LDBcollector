dcd2b24a50d06b7956c91aab44b9623b8e7d4c3b
Sebastian Schuberth <sebastian@doubleopen.org>
2023-12-15 11:26:53 +0100

    feat(scripts): Add a check for valid license grammar syntax
    This could be made stricter on demand later on.
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- scripts/check-license-classifications.main.kts
+++ scripts/check-license-classifications.main.kts
@@ -10,6 +10,7 @@ import kotlin.system.exitProcess
 
 import org.ossreviewtoolkit.model.licenses.LicenseClassifications
 import org.ossreviewtoolkit.model.readValue
+import org.ossreviewtoolkit.utils.spdx.SpdxExpression.Strictness
 
 val scriptsDir = __FILE__.parentFile
 val licenseClassificationsFile = scriptsDir.resolve("../license-classifications.yml").canonicalFile
@@ -19,6 +20,16 @@ val licenseClassifications = runCatching {
 }.onFailure {
     println("Unable to read '$licenseClassificationsFile': ${it.message}")
 }.getOrElse {
+    exitProcess(1)
+}
+
+val (validLicenses, invalidLicenses) = licenseClassifications.categoriesByLicense.keys.partition {
+    it.isValid(Strictness.ALLOW_ANY)
+}
+
+if (invalidLicenses.isNotEmpty()) {
+    println("The following licenses cannot be parsed as SPDX license expressions:")
+    println(invalidLicenses.joinToString("\n"))
     exitProcess(1)
 }
 
