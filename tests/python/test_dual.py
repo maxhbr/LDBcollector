6754bf1adb7f0b22f2d5270e58c8ee5c4eada024
Henrik Sandklef <hesa@sandklef.com>
2023-12-17 22:43:42 +0100

    test: add dual license tests

---
+++ tests/python/test_dual.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2023 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import os
+import re
+import sys
+import pytest
+
+from flame.license_db import FossLicenses
+from flame.license_db import Validation
+from flame.exception import FlameException
+import logging
+
+fl = FossLicenses(license_dir="tests/licenses", logging_level=logging.INFO)
+
+def test_no_dual():
+    lic = fl.expression_license("GPL-2.0-or-later", update_dual=False)
+    assert lic['identified_license'] == "GPL-2.0-or-later"
+
+def test_dual():
+    lic = fl.expression_license("GPL-2.0-or-later", update_dual=True)
+    assert lic['identified_license'] == "GPL-2.0-only OR GPL-3.0-only"
+
+def test_dual_implicit():
+    lic = fl.expression_license("GPL-2.0-or-later")
+    assert lic['identified_license'] == "GPL-2.0-only OR GPL-3.0-only"
+
+def test_dual_complex():
+    lic = fl.expression_license("GPL-2.0-or-later OR MIT")
+    assert lic['identified_license'] == "(GPL-2.0-only OR GPL-3.0-only) OR MIT"
+
+def test_dual_complex():
+    lic = fl.expression_license("GPL-2.0-or-later AND MIT")
+    assert lic['identified_license'] == "(GPL-2.0-only OR GPL-3.0-only) AND MIT"
+
+def test_dual_complexer():
+    lic = fl.expression_license("X11 OR GPL-2.0-or-later AND MIT")
+    assert lic['identified_license'] == "X11 OR ((GPL-2.0-only OR GPL-3.0-only) AND MIT)"
+
+
