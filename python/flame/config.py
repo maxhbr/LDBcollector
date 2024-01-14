726b4f683f2b711b2024c36896142b9442e1f0d2
Henrik Sandklef <hesa@sandklef.com>
2024-01-14 21:09:47 +0100

    new version schema

--- python/flame/config.py
+++ python/flame/config.py
@@ -7,7 +7,7 @@ import logging
 import os
 import flame.exception
 
-SW_VERSION = "0.1.9"
+SW_VERSION = "0.19.0"
 
 PYTHON_DIR = os.path.dirname(os.path.realpath(__file__))
 VAR_DIR = os.path.join(PYTHON_DIR, 'var')
