4313ba69056f7b75c9621ee2a4442df795e79e39
Peter Monks <pmonks@gmail.com>
2024-01-28 13:12:04 -0800

    :fire: Remove redundant eastwood appeasement

--- pbr.clj
+++ pbr.clj
@@ -22,7 +22,6 @@
   (assoc opts
          :lib          'com.github.pmonks/lice-comb
          :version      (pbr/calculate-version 2 0)
-;         :version      (format "2.0.%s-RCx-SNAPSHOT" (b/git-count-revs nil))
          :write-pom    true
          :validate-pom true
          :pom          {:description      "A Clojure library for software license detection."
@@ -31,5 +30,4 @@
                         :developers       [:developer {:id "pmonks" :name "Peter Monks" :email "pmonks+lice-comb@gmail.com"}]
                         :scm              {:url "https://github.com/pmonks/lice-comb" :connection "scm:git:git://github.com/pmonks/lice-comb.git" :developer-connection "scm:git:ssh://git@github.com/pmonks/lice-comb.git"}
                         :issue-management {:system "github" :url "https://github.com/pmonks/lice-comb/issues"}}
-         :codox        {:namespaces ['lice-comb.deps 'lice-comb.files 'lice-comb.lein 'lice-comb.matching 'lice-comb.maven 'lice-comb.utils]}
-         :eastwood     {:exclude-linters [:unused-ret-vals-in-try :no-ns-form-found]}))
+         :codox        {:namespaces ['lice-comb.deps 'lice-comb.files 'lice-comb.lein 'lice-comb.matching 'lice-comb.maven 'lice-comb.utils]}))
