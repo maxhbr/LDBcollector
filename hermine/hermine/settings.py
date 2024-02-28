d5419e226a23cfb10856e34452a27e8b4adfcb52
Maud Royer <hello@maudroyer.fr>
2024-02-28 16:02:19 +0100

    feat: beautiful forms
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

--- hermine/hermine/settings.py
+++ hermine/hermine/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.forms",
     "social_django",
     "rest_framework",
     "rest_framework.authtoken",
@@ -100,6 +101,8 @@ TEMPLATES = [
         },
     }
 ]
+
+FORM_RENDERER = "django.forms.renderers.TemplatesSetting"
 
 MESSAGE_TAGS = {
     messages.INFO: "primary",
