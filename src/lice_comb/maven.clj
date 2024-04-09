4f05025726f7140225f6e786d0e07e8d0dea4605
Peter Monks <pmonks@gmail.com>
2024-04-09 11:15:29 -0700

    :books: Fix comment

--- src/lice_comb/maven.clj
+++ src/lice_comb/maven.clj
@@ -119,11 +119,11 @@
   "Attempts to determine the license(s) (a map) from a POM license name/URL
   pair. Returns nil if no matches were found."
   [{:keys [name url]}]
-  ; 1. Look in the name field(s)
+  ; 1. Look in the name field
   (let [name-expressions (lcm/name->expressions-info name)]
     (if (or (empty? name-expressions)
             (and (= 1 (count name-expressions)) (lcm/unidentified? (first (keys name-expressions)))))
-      ; 2. If the names didn't give us any identified licenses, look in the url field(s) (this can be slower and less accurate, which is why it has lower priority)
+      ; 2. If the names didn't give us any identified licenses, look in the url field (this can be slower and less accurate, which is why it has lower priority)
       (let [uri-expressions (lcm/uri->expressions-info url)]
         (if (or (empty? uri-expressions)
                 (and (= 1 (count uri-expressions)) (lcm/unidentified? (first (keys uri-expressions)))))
