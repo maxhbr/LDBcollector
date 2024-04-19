4d046e1addb18df06c0c91faedafd42ee3435930
Henrik Sandklef <hesa@sandklef.com>
2024-04-19 09:26:41 +0200

    add IJG tests

--- tests/shell/sanity-check.sh
+++ tests/shell/sanity-check.sh
@@ -174,12 +174,16 @@ check_presence GPL-3.0-or-later " -e 3 -e later" " -e '1 ' -e 2"
 check_presence AGPL-3.0-only     " -e 3 " " -e '1 ' -e 2 -e later -e plus -e +"
 check_presence AGPL-3.0-or-later " -e 3 -e later -e +" " -e '1 ' -e 2"
 
+check_presence IJG " -i -e ijg -e independent -e jpeg " ""
+
 check_presence LGPL-2.0-only " -e 2.0 " " -e 3  -e later"
 check_presence LGPL-2.1-only " -e 2.1 " " -e 3  -e later"
 check_presence LGPL-3.0-only " -e 3 " " -e 2  -e later"
 
 check_presence LGPL-2.1-or-later " -e 2 -e later" " -e 3"
 check_presence LGPL-3.0-or-later " -e 3 -e later" " -e 2"
+
+check_presence LicenseRef-scancode-g10-permissive " -i -e  g10 " ""
 
 check_presence MIT " -i -e MIT -e Expat" " -e 0"
 check_presence MIT-0 " -e 0 -i -e \"no attribution\"" ""
