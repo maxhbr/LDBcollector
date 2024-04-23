8f0ca3c79822a3d80deb652c0b367c865f726e75
Maximilian Huber <gh@maxhbr.de>
2024-04-23 10:32:57 +0200

    cleanup duplicate
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- data/update.oslic.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd "$(dirname "$0")/.."
-if ! git ls-remote --exit-code oslic; then
-  git remote add -f oslic https://github.com/telekom/oslic
-else
-  git fetch "https://github.com/telekom/oslic"
-fi
-git subtree pull --prefix data/oslic oslic master
