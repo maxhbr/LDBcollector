415a5ff25355b5c8e1a12b8ab5b5c15756541b05
Peter Monks <pmonks@gmail.com>
2024-08-26 09:42:09 -0700

    :umbrella: Fix incomplete unit tests

--- test/lice_comb/matching_test.clj
+++ test/lice_comb/matching_test.clj
@@ -162,6 +162,7 @@
     (is (valid= #{"Apache-2.0" "GPL-3.0-only WITH Classpath-exception-2.0"} (name->expressions "Apache License version 2.0 / GNU General Public License version 3 with classpath exception")))
     (is (valid= #{"EPL-2.0 OR (Apache-2.0 AND BSD-3-Clause) OR (GPL-2.0-or-later WITH Classpath-exception-2.0 AND MIT)"} (name->expressions "Eclipse Public License or General Public License 2.0 or (at your discretion) later w/ classpath exception aNd MIT Licence or three clause bsd and Apache Licence"))))
   (testing "Listed names"
+    ; We use `some` here, because some license names resolve to multiple licenses ids
     (run! #(is (some #{(:id %)} (name->expressions (:name %)))) @lcis/license-list-d)
     (run! #(is (some #{(:id %)} (name->expressions (:name %)))) @lcis/exception-list-d))
   (testing "Names seen in handpicked POMs on Maven Central"
