ab9aae74d0c9f48200dc0de498b501e357fa3c7b
hesa <hesa@localhost>
2024-09-07 15:09:17 +0000

    Release 0.20.4

--- python/flame/config.py
+++ python/flame/config.py
@@ -7,7 +7,7 @@ import logging
 import os
 import flame.exception
 
-SW_VERSION = '0.20.3'
+SW_VERSION = '0.20.4'
 
 PYTHON_DIR = os.path.dirname(os.path.realpath(__file__))
 VAR_DIR = os.path.join(PYTHON_DIR, 'var')
