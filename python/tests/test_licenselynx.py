4a3d8821fafa5f929d9a64e7d6dfaadd98fac2a1
Leo Reinmann <leo.reinmann@siemens.com>
2024-10-21 16:09:21 +0200

    fix(python): change test code for failing test case

--- python/tests/test_licenselynx.py
+++ python/tests/test_licenselynx.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 from unittest.mock import mock_open, patch, MagicMock
-from licenselynx.licenselynx import LicenseLynx
+from licenselynx.licenselynx import LicenseLynx, LicenseObject
 
 
 @pytest.fixture
@@ -10,12 +10,23 @@ def mock_json_data():
     return '{"Academic Free License v2.1": {"canonical": "AFL-2.1","src": "spdx"}}'
 
 
-def test_map_with_existing_license(mock_json_data):
-    with patch('builtins.open', mock_open(read_data=mock_json_data)):
+def test_map_with_existing_license():
+    mock_data = json.dumps({
+        "MIT": {
+            "canonical": "MIT License",
+            "src": "opensource.org/licenses/MIT"
+        }
+    })
+    mock_file = MagicMock()
+    mock_file.__enter__.return_value = mock_open(read_data=mock_data).return_value
+
+    with patch('importlib.resources.files') as mock_resources_files:
+        mock_resources_files.return_value.joinpath.return_value.open.return_value = mock_file
         license_lynx = LicenseLynx()
-        result = license_lynx.map("Academic Free License v2.1")
-        assert result.canonical == "AFL-2.1"
-        assert result.src == "spdx"
+        result = license_lynx.map("MIT")
+        assert isinstance(result, LicenseObject)
+        assert result.canonical == "MIT License"
+        assert result.src == "opensource.org/licenses/MIT"
 
 
 def test_map_with_non_existing_license():
