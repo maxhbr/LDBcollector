44976899cccf95658832f2c58a006fe65ba7dbcb
Maud Royer <hello@maudroyer.fr>
2025-02-10 14:54:13 +0100

    fix: wait domcontentloaded for installing events
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

--- hermine/vite_modules/src/hermine/main.js
+++ hermine/vite_modules/src/hermine/main.js
@@ -11,45 +11,56 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 import './fonts/nunito.css'
 import './theme/main.scss'
 
-/* Aside: submenus toggle */
-Array.from(document.getElementsByClassName('menu is-menu-main')).forEach(function (el) {
-  Array.from(el.getElementsByClassName('has-dropdown-icon')).forEach(function (elA) {
-    elA.addEventListener('click', function (e) {
-      var dropdownIcon = e.currentTarget.getElementsByClassName('dropdown-icon')[0].getElementsByClassName('fas')[0];
-      e.currentTarget.parentNode.classList.toggle('is-active');
-      dropdownIcon.classList.toggle('fa-caret-right');
-      dropdownIcon.classList.toggle('fa-caret-down');
+document.addEventListener('DOMContentLoaded', function () {
+  /* Aside: submenus toggle */
+  Array.from(document.getElementsByClassName('menu is-menu-main')).
+    forEach(function (el) {
+      Array.from(el.getElementsByClassName('has-dropdown-icon')).
+        forEach(function (elA) {
+          elA.addEventListener('click', function (e) {
+            var dropdownIcon = e.currentTarget.getElementsByClassName(
+              'dropdown-icon')[0].getElementsByClassName('fas')[0];
+            e.currentTarget.parentNode.classList.toggle('is-active');
+            dropdownIcon.classList.toggle('fa-caret-right');
+            dropdownIcon.classList.toggle('fa-caret-down');
+          });
+        });
     });
-  });
-});
-/* Aside Mobile toggle */
+  /* Aside Mobile toggle */
 
-Array.from(document.getElementsByClassName('jb-aside-mobile-toggle')).forEach(function (el) {
-  el.addEventListener('click', function (e) {
-    var dropdownIcon = e.currentTarget.getElementsByClassName('icon')[0].getElementsByClassName('mdi')[0];
-    document.documentElement.classList.toggle('has-aside-mobile-expanded');
-    dropdownIcon.classList.toggle('mdi-forwardburger');
-    dropdownIcon.classList.toggle('mdi-backburger');
-  });
-});
-/* NavBar menu mobile toggle */
-
-Array.from(document.getElementsByClassName('jb-navbar-menu-toggle')).forEach(function (el) {
-  el.addEventListener('click', function (e) {
-    var dropdownIcon = e.currentTarget.getElementsByClassName('icon')[0].getElementsByClassName('mdi')[0];
-    document.getElementById(e.currentTarget.getAttribute('data-target')).classList.toggle('is-active');
-    dropdownIcon.classList.toggle('mdi-dots-vertical');
-    dropdownIcon.classList.toggle('mdi-close');
-  });
-});
-
-
-/* Filter form application */
-Array.from(document.getElementsByClassName('hermine-filter-form')).forEach(function (el) {
-  Array.from(el.getElementsByTagName('select')).forEach(function (select) {
-    select.addEventListener('change', function (e) {
-      el.submit();
+  Array.from(document.getElementsByClassName('jb-aside-mobile-toggle')).
+    forEach(function (el) {
+      el.addEventListener('click', function (e) {
+        var dropdownIcon = e.currentTarget.getElementsByClassName(
+          'icon')[0].getElementsByClassName('mdi')[0];
+        document.documentElement.classList.toggle('has-aside-mobile-expanded');
+        dropdownIcon.classList.toggle('mdi-forwardburger');
+        dropdownIcon.classList.toggle('mdi-backburger');
+      });
     });
-  })
+  /* NavBar menu mobile toggle */
+
+  Array.from(document.getElementsByClassName('jb-navbar-menu-toggle')).
+    forEach(function (el) {
+      el.addEventListener('click', function (e) {
+        var dropdownIcon = e.currentTarget.getElementsByClassName(
+          'icon')[0].getElementsByClassName('mdi')[0];
+        document.getElementById(e.currentTarget.getAttribute('data-target')).
+          classList.
+          toggle('is-active');
+        dropdownIcon.classList.toggle('mdi-dots-vertical');
+        dropdownIcon.classList.toggle('mdi-close');
+      });
+    });
+
+  /* Filter form application */
+  Array.from(document.getElementsByClassName('hermine-filter-form')).
+    forEach(function (el) {
+      Array.from(el.getElementsByTagName('select')).forEach(function (select) {
+        select.addEventListener('change', function (e) {
+          el.submit();
+        });
+      })
+    })
 })
 
