8a8d623c8b1bdd857efd4e01adf327409afbb6ab
Leo Reinmann <leo.reinmann@siemens.com>
2025-02-11 12:25:47 +0100

    refactor(python): improve map function and .gitlab-ci.yml

---
+++ python/licenselynx/license_object.py
@@ -0,0 +1,4 @@
+class LicenseObject:
+    def __init__(self, canonical, src):
+        self.canonical = canonical
+        self.src = src
--- python/licenselynx/licenselynx.py
+++ python/licenselynx/licenselynx.py
@@ -1,31 +1,32 @@
 import json
+import sys
 from importlib import resources
+from typing import Optional
+from licenselynx.license_object import LicenseObject
 
 
 class LicenseLynx:
-    @staticmethod
-    def map(license_name: str):
+    def __init__(self):
+        self._file_path = resources.files("licenselynx.resources").joinpath("merged_data.json")
+        try:
+            with self._file_path.open() as file:
+                self._merged_data = json.load(file)
+        except Exception as e:
+            raise e.with_traceback(sys.exc_info()[2])
+
+    def map(self, license_name: str) -> Optional[LicenseObject]:
         """
         Maps license name to the canonical license identifier
         :param license_name: string of a license name
-        :return: LicenseObject with the canonical license identifier and source or None if no license is found
+        :return: LicenseObject with the canonical license identifier and source, None if no license is found,
+        or throws an exception if a runtime error occurs
         """
         try:
-            file_path = resources.files("licenselynx.resources").joinpath("merged_data.json")
+            entry = self._merged_data.get(license_name, None)
 
-            with file_path.open() as file:
-                merged_data = json.load(file)
-
-            entry = merged_data.get(license_name)
-            if entry:
-                return LicenseObject(canonical=entry.get("canonical"), src=entry.get("src"))
-            else:
+            if not entry:
                 return None
+
+            return LicenseObject(canonical=entry.get("canonical"), src=entry.get("src"))
         except Exception as e:
-            return {"error": f"Error reading or parsing file: {str(e)}"}
-
-
-class LicenseObject:
-    def __init__(self, canonical, src):
-        self.canonical = canonical
-        self.src = src
+            raise e.with_traceback(sys.exc_info()[2])
