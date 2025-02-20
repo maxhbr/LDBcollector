e1056e11888b9bda96697375c36edfe1d8f7ad97
Leo Reinmann <leo.reinmann@siemens.com>
2025-02-20 09:52:53 +0100

    tests(java): add dummy data for testing

--- java/src/test/java/org/licenselynx/LicenseLynxTest.java
+++ java/src/test/java/org/licenselynx/LicenseLynxTest.java
@@ -3,6 +3,7 @@ package com.siemens.licenselynx;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.Assertions;
 
@@ -52,18 +53,6 @@ class LicenseLynxTest
 
 
     @Test
-    void testWithNullValuesInLicenseMap()
-    {
-        Map<String, LicenseObject> testMap = new HashMap<>();
-        testMap.put("test", new LicenseObject(null, null));
-        LicenseMapSingleton testInstance = new LicenseMapSingleton(testMap);
-
-        Assertions.assertNull(testInstance.getLicenseMap().get("test").getCanonical());
-    }
-
-
-
-    @Test
     void testValidSingletonInstance()
     {
         // Ensure getInstance() returns the same object
@@ -72,6 +61,8 @@ class LicenseLynxTest
 
         Assertions.assertSame(instance1, instance2, "getInstance() should always return the same instance");
     }
+
+
 
     @Test
     void testNullInputStream()
@@ -89,8 +80,10 @@ class LicenseLynxTest
         LicenseDataLoader loader = new LicenseDataLoader(new ObjectMapper(), mockClassLoader);
 
         // Act && Assert
-        Assertions.assertThrows(IllegalArgumentException.class, () -> loader.loadLicenses());
+        Assertions.assertThrows(IllegalArgumentException.class, loader::loadLicenses);
     }
+
+
 
     @Test
     void testIOException()
@@ -116,7 +109,6 @@ class LicenseLynxTest
         LicenseDataLoader loader = new LicenseDataLoader(new ObjectMapper(), mockClassLoader);
 
         // Act && Assert
-        Assertions.assertThrows(UncheckedIOException.class, () -> loader.loadLicenses());
+        Assertions.assertThrows(UncheckedIOException.class, loader::loadLicenses);
     }
-
 }
