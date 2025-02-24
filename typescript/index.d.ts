5b5cdb3fefce98d5cbaf203f48f09cf12ab7c82d
Leo Reinmann <leo.reinmann@siemens.com>
2025-02-24 15:25:37 +0100

    chore(typescript): auto-generate index.d.ts

--- typescript/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@sbom/licenselynx' {
-    export interface LicenseObject {
-        readonly canonical: string;
-        readonly src: string;
-    }
-
-    export function map(licenseName: string): LicenseObject;
-}
