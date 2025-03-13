6b0c60346ae7e8bdc019b49b7bad551f44b7d4ef
Henrik Sandklef <hesa@sandklef.com>
2025-03-13 23:34:46 +0100

    add separators for now

--- tests/python/test_operators.py
+++ tests/python/test_operators.py
@@ -7,6 +7,7 @@ import pytest
 from flame.license_db import FossLicenses
 from flame.exception import FlameException
 import logging
+import sys
 
 fl = FossLicenses(config={
     'level': 'logging.INFO'})
@@ -14,24 +15,31 @@ fl = FossLicenses(config={
 logger = logging.getLogger("test_values")
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
 
-def __test_impl(lic_expr, expected_lic):
+def __test_impl_sub(lic_expr, expected_lic):
     lic = fl.expression_license(lic_expr, update_dual=False)
+    #print(f'lic: {lic}', file=sys.stderr)
     logging.debug(f'assert {lic["identified_license"]} == {expected_lic}')
     assert lic['identified_license'] == expected_lic
+    print(f'OK: {lic}', file=sys.stderr)
     
+def __test_impl(lic_expr1, lic_op, lic_expr2, expected_lic, addons):
+    for pre in addons:
+        for post in addons:
+            print(f'TEST: {lic_expr1}{pre}{lic_op}{post}{lic_expr2}', file=sys.stderr)
+            __test_impl_sub(f'{lic_expr1}{pre}{lic_op}{post}{lic_expr2}', expected_lic)
 
-def test_and_mit_gpl2_1():
-    __test_impl("mit and GPLv2", 'MIT AND GPL-2.0-only')
-    __test_impl("mit && GPLv2", 'MIT AND GPL-2.0-only')
-    __test_impl("mit & GPLv2", 'MIT AND GPL-2.0-only')
-    __test_impl("mit AND GPLv2", 'MIT AND GPL-2.0-only')
 
-def test_or_mit0_gpl2_1():
+def test_and_mit_gpl2():
+    for op in [' AND ', '<AND>', ' & ', ' && ']:
+        __test_impl('mit', op, 'GPLv2', 'MIT AND GPL-2.0-only', ['', ' '])
+
+
+def _test_or_mit0_gpl2_1():
     __test_impl("mit-0 or GPLv2", 'MIT-0 OR GPL-2.0-only')
     __test_impl("mit-0 || GPLv2", 'MIT-0 OR GPL-2.0-only')
     __test_impl("mit-0 | GPLv2", 'MIT-0 OR GPL-2.0-only')
     __test_impl("mit-0 OR GPLv2", 'MIT-0 OR GPL-2.0-only')
-def test_or_mit0_gpl2_2():
+def _test_or_mit0_gpl2_2():
     __test_impl("mit-0 <OR> GPLv2", 'MIT-0 OR GPL-2.0-only')
     __test_impl("mit-0<OR> GPLv2", 'MIT-0 OR GPL-2.0-only')
 
