d4b249da035db4f5a03c411fab2fe52a6140cbda
Camille Moulin <cmoulin@inno3.fr>
2025-04-01 21:26:59 +0200

    fix(tests):Fix reverse address
    Signed-off-by: Camille Moulin <cmoulin@inno3.fr>

--- hermine/cube/tests/api/test_validation.py
+++ hermine/cube/tests/api/test_validation.py
@@ -92,7 +92,9 @@ class ReleaseStepsAPITestCase(BaseHermineAPITestCase):
         self.create_derogations()
         self.import_sbom()
 
-        res = self.client.get(reverse("cube:release_validation", kwargs={"pk": 1}))
+        res = self.client.get(
+            reverse("cube:release_validation_step_1", kwargs={"pk": 1})
+        )
         self.assertEqual(res.status_code, 200)
         self.assertEqual(res.context["object"].valid_step, STEP_POLICY)
         self.assertEqual(len(res.context["derogations"]), 1)
