3b42c57b02398be79d3956158c55a4492d91887a
Sebastian Schuberth <sebastian@doubleopen.org>
2025-06-26 15:16:56 +0200

    fix(gradle): Include the `io.atlassian` repository
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
         }
 
         filter {
-            includeGroupByRegex("com\\.atlassian\\..*")
+            includeGroupByRegex("(com|io)\\.atlassian\\..*")
             includeVersionByRegex("log4j", "log4j", ".*-atlassian-.*")
         }
     }
