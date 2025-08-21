93403313db709610752c2ddd6fe96121766ac56c
Kaushlendra Pratap <kaushlendra-pratap.singh@siemens.com>
2025-08-21 22:47:09 +0530

    fix(docker): Migration and Basic updates in Docker Build
    Signed-off-by: Kaushlendra Pratap <kaushlendra-pratap.singh@siemens.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: 2024 Kaushlendra Pratap <kaushlendra-pratap.singh@siemens.com>
 # SPDX-License-Identifier: GPL-2.0-only
 
-set -e
+set -o errexit -o nounset -o pipefail
 
 db_host="${DB_HOST:-localhost}"
 db_port="${DB_PORT:-5432}"
@@ -12,8 +12,45 @@ db_password="${DB_PASSWORD:-fossy}"
 populate_db="${POPULATE_DB:-true}"
 data_file="/app/licenseRef.json"
 
-printf "READ_API_AUTHENTICATION_ENABLED=false\nTOKEN_HOUR_LIFESPAN=24\nAPI_SECRET=%s\n" $(openssl rand -hex 32) > /app/.env
+entry_level_user_name="fossy_super_admin"
+entry_level_user_password="fossy_super_admin"
+entry_level_user_display_name="fossy_super_admin"
+entry_level_user_email="fossy_super_admin@example.org"
 
-/app/laas -host=$db_host -port=$db_port -user=$db_user -dbname=$db_name \
-  -password=$db_password -datafile="$data_file" -populatedb=$populate_db
+#sed -i "s|^API_SECRET=.*|API_SECRET=$(openssl rand -hex 32)|" /app/.env
+echo "API_SECRET=$(openssl rand -hex 32)" >> /app/.env
 
+echo "Setting up PostgreSQL database..."
+PGPASSWORD=$db_password psql -h "$db_host" -U "$db_user" -p "$db_port" -d "$db_name"<<EOF
+DO \$\$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '${db_name}') THEN
+      CREATE DATABASE ${db_name};
+   END IF;
+END
+\$\$;
+DO \$\$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '${db_user}') THEN
+      CREATE USER ${db_user} WITH PASSWORD '${db_password}';
+   END IF;
+END
+\$\$;
+GRANT ALL PRIVILEGES ON DATABASE ${db_name} TO ${db_user};
+ALTER DATABASE ${db_name} OWNER TO ${db_user};
+EOF
+
+echo "Database setup done!"
+
+echo "Running database migrations..."
+migrate -path /app/pkg/db/migrations -database "postgres://$db_user:$db_password@$db_host:$db_port/$db_name?sslmode=disable" up
+
+echo "Inserting initial SUPER_ADMIN user..."
+
+PGPASSWORD="$db_password" psql -h "$db_host" -U "$db_user" -p "$db_port" -d "$db_name" -c \
+"INSERT INTO users (user_name, user_password, user_level, display_name, user_email)
+ SELECT '$entry_level_user_name', '$entry_level_user_password', 'SUPER_ADMIN', '$entry_level_user_display_name', '$entry_level_user_email'
+ WHERE NOT EXISTS (SELECT 1 FROM users WHERE user_level='SUPER_ADMIN');"
+
+echo "Starting LAAS service..."
+/app/laas
