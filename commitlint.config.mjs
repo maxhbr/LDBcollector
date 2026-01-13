cf2cfb3047c1bd8a20334cc692fe3028edeaa979
Vishesh Gupta <vishesh15th@gmail.com>
2026-01-13 16:13:30 +0530

    feat(ci): add CI check for commit linting
    Signed-off-by: Vishesh Gupta <vishesh15th@gmail.com>

---
+++ commitlint.config.mjs
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: 2026 Vishesh Gupta <vishesh15th@gmail.com>
+// SPDX-License-Identifier: GPL-2.0-only
+export default {
+  extends: ["@commitlint/config-conventional"],
+};
