74edeadc0c9eeb81a2b8e1ecd4b3fa45fc3b306b
Maximilian Huber <gh@maxhbr.de>
2026-02-18 14:54:13 +0100

    update
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- data/all_update.sh
+++ data/all_update.sh
@@ -2,6 +2,11 @@
 
 set -uo pipefail
 
+if git diff --quiet; then
+    echo "Working tree has modifications. Please commit or stash your changes before running this script."
+    exit 1
+fi
+
 failed_scripts=()
 
 # Find and execute all update.*.sh scripts
@@ -11,11 +16,10 @@ while IFS= read -r script; do
         failed_scripts+=("$script")
     fi
     
-    if git diff --quiet || { sleep 1 && git diff --quiet; }; then
-        pwd
-        echo "Working tree has modifications. Please commit or stash your changes before running this script."
-        exit 1
-    fi
+    # if git diff --quiet; then
+    #     echo "Working tree has modifications. Please commit or stash your changes before running this script."
+    #     exit 1
+    # fi
 done < <(find "$(dirname "$0")" \
     -mindepth 1 \
     -maxdepth 1 \
