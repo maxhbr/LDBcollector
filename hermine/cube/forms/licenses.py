fe795aebda7f4f290b12ab287a5facc03430bfb9
Maud Royer <hello@maudroyer.fr>
2026-03-19 15:54:25 +0100

    fix: fix some edge-cases of license-related form broken validation
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

--- hermine/cube/forms/licenses.py
+++ hermine/cube/forms/licenses.py
@@ -2,7 +2,7 @@
 #
 #  SPDX-License-Identifier: AGPL-3.0-only
 from django.db import transaction
-from django.forms import ModelForm, ModelChoiceField, Form
+from django.forms import ModelForm, ModelChoiceField, Form, HiddenInput
 
 from cube.forms.mixins import AutocompleteFormMixin
 from cube.models import Generic, Obligation, License, Compatibility
@@ -155,5 +155,6 @@ class ObligationForm(AutocompleteFormMixin, ModelForm):
 class CompatibilityForm(AutocompleteFormMixin, ModelForm):
     class Meta:
         model = Compatibility
-        fields = ("to_license", "direction")
+        fields = ("from_license", "to_license", "direction")
         autocomplete_fields = ["to_license"]
+        widgets = {"from_license": HiddenInput()}
