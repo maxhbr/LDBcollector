894e6cbcfb10764c3d11dcaab16200b48ac5a85e
Maximilian Huber <gh@maxhbr.de>
2026-06-23 11:43:16 +0200

    source/OSI: migrate to new API at opensource.org/api/license
    The old api.opensource.org domain has been taken over by a WordPress
    blog and no longer serves JSON. OSI published a new API at
    https://opensource.org/api/license (announced 2025-06-19).
    
    - Replace the dead 'opensource' Hackage library with direct http-conduit
      + aeson calls against the new endpoint
    - Introduce OSILicenseV2 / OSILinks data types matching the new JSON
      schema (id, name, spdx_id, approved, keywords, license_steward_url,
      _links)
    - Update getApplicableLNs: osi: namespace from id, plain LN from name,
      SPDX: namespace from spdx_id
    - Update getImpliedStmts: approved bool, steward URL, OSI page URL from
      _links.html.href, new keyword vocabulary
    - Update keyword mapping for new terms (popular-strong-community,
      redundant-with-more-popular, voluntarily-retired, superseded, …)
    - Update guardSource to probe the new endpoint and validate HTTP 200
    - Update getOriginalData URLs
    - Drop 'opensource' dependency from all three cabal stanzas

--- src/Ldbcollector/Source/OSI.hs
+++ src/Ldbcollector/Source/OSI.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Ldbcollector.Source.OSI
   ( OSI (..),
@@ -7,74 +8,131 @@ module Ldbcollector.Source.OSI
   )
 where
 
-import Control.Monad.Except (runExceptT)
+import Control.Monad.Except (ExceptT, runExceptT, throwError)
+import Data.Aeson (eitherDecode)
+import Data.ByteString.Lazy qualified as BL
 import Data.Text qualified as T
 import Data.Vector qualified as V
 import Ldbcollector.Model
-import Network.Protocol.OpenSource.License qualified as OSI
-import Network.HTTP.Simple (httpLBS, getResponseStatusCode, parseRequest, Response)
+import Network.HTTP.Simple (httpLBS, getResponseBody, getResponseStatusCode, parseRequest, Response)
 import Control.Exception (try, SomeException)
-
-newtype OSILicense
-  = OSILicense OSI.OSILicense
-  deriving (Eq, Show, Generic)
-
-instance ToJSON OSILicense
 
 isOsiApproved :: Maybe Bool -> LicenseStatement
 isOsiApproved (Just True) = LicenseRating $ PositiveLicenseRating (ScopedLicenseTag "OSI" "Approved" NoLicenseTagText)
 isOsiApproved (Just False) = LicenseRating $ NegativeLicenseRating (ScopedLicenseTag "OSI" "Rejected" NoLicenseTagText)
 isOsiApproved Nothing = LicenseRating $ NegativeLicenseRating (ScopedLicenseTag "OSI" "Not-Approved" NoLicenseTagText)
 
+-- | Represents a license entry from the new OSI API at
+--   https://opensource.org/api/license
+data OSILinks = OSILinks
+  { osiLinksHtml :: Maybe Text
+  }
+  deriving (Eq, Show, Generic)
+
+instance FromJSON OSILinks where
+  parseJSON = withObject "OSILinks" $ \o -> do
+    html <- o .:? "html"
+    href <- case html of
+      Nothing -> pure Nothing
+      Just h  -> h .:? "href"
+    return $ OSILinks href
+
+instance ToJSON OSILinks where
+  toJSON (OSILinks href) = object ["html" .= object ["href" .= href]]
+
+data OSILicenseV2 = OSILicenseV2
+  { osiId               :: Text
+  , osiName             :: Text
+  , osiSpdxId           :: Maybe Text
+  , osiApproved         :: Maybe Bool
+  , osiKeywords         :: [Text]
+  , osiLicenseStewardUrl :: Maybe Text
+  , osiLinks            :: Maybe OSILinks
+  }
+  deriving (Eq, Show, Generic)
+
+instance FromJSON OSILicenseV2 where
+  parseJSON = withObject "OSILicenseV2" $ \o -> do
+    osiId               <- o .: "id"
+    osiName             <- o .: "name"
+    osiSpdxId           <- o .:? "spdx_id"
+    osiApproved         <- o .:? "approved"
+    osiKeywords         <- o .:? "keywords" .!= []
+    osiLicenseStewardUrl <- do
+      url <- o .:? "license_steward_url"
+      return $ case url of
+        Just u | not (T.null u) -> Just u
+        _                       -> Nothing
+    osiLinks            <- o .:? "_links"
+    return OSILicenseV2 {..}
+
+instance ToJSON OSILicenseV2
+
+newtype OSILicense
+  = OSILicense OSILicenseV2
+  deriving (Eq, Show, Generic)
+
+instance ToJSON OSILicense
+
 instance LicenseFactC OSILicense where
   getType _ = "OSILicense"
   getApplicableLNs (OSILicense l) =
-    (LN . newNLN "osi" . OSI.olId) l
-      `AlternativeLNs` ((LN . newLN . OSI.olName) l : map (\i -> LN $ newNLN (OSI.oiScheme i) (OSI.oiIdentifier i)) (OSI.olIdentifiers l))
-      `ImpreciseLNs` map (LN . newLN . OSI.oonName) (OSI.olOther_names l)
-  getImpliedStmts osil@(OSILicense l) =
-    let urls = (map (\link -> (LicenseUrl ((Just . unpack . OSI.olNote) link) . unpack . OSI.olUrl) link) . OSI.olLinks) l
-        keywords =
-          map
-            ( \kw -> case kw of
-                "osi-approved" -> isOsiApproved (Just True)
-                "discouraged" -> LicenseRating $ NegativeLicenseRating (ScopedLicenseTag "OSI-keyword" kw NoLicenseTagText)
-                "non-reusable" -> LicenseRating $ NegativeLicenseRating (ScopedLicenseTag "OSI-keyword" kw NoLicenseTagText)
-                "retired" -> LicenseRating $ NegativeLicenseRating (ScopedLicenseTag "OSI-keyword" kw NoLicenseTagText)
-                "redundant" -> LicenseRating $ NegativeLicenseRating (ScopedLicenseTag "OSI-keyword" kw NoLicenseTagText)
-                "popular" -> LicenseRating $ PositiveLicenseRating (ScopedLicenseTag "OSI-keyword" kw NoLicenseTagText)
-                "permissive" -> LicenseType Permissive
-                "copyleft" -> LicenseType Copyleft
-                "special-purpose" -> LicenseRating $ NeutralLicenseRating (ScopedLicenseTag "OSI-keyword" kw NoLicenseTagText)
-                _ -> (stmt . T.unpack) kw
-            )
-            (OSI.olKeywords l)
-     in urls ++ keywords
+    LN (newNLN "osi" (osiId l))
+      `AlternativeLNs` ( LN (newLN (osiName l))
+                           : maybe [] (\s -> [LN (newNLN "SPDX" s)]) (osiSpdxId l)
+                       )
+  getImpliedStmts (OSILicense l) =
+    let approvalStmt = isOsiApproved (osiApproved l)
+        stewardUrl   = maybe [] (\u -> [LicenseUrl (Just "License Steward") (T.unpack u)]) (osiLicenseStewardUrl l)
+        htmlUrl      = maybe [] (\u -> [LicenseUrl (Just "OSI Page") (T.unpack u)])
+                         (osiLinks l >>= osiLinksHtml)
+        keywords     = map keywordToStmt (osiKeywords l)
+     in [approvalStmt] ++ stewardUrl ++ htmlUrl ++ keywords
+
+keywordToStmt :: Text -> LicenseStatement
+keywordToStmt kw = case kw of
+  "popular-strong-community"  -> LicenseRating $ PositiveLicenseRating  (ScopedLicenseTag "OSI-keyword" kw NoLicenseTagText)
+  "redundant-with-more-popular" -> LicenseRating $ NegativeLicenseRating (ScopedLicenseTag "OSI-keyword" kw NoLicenseTagText)
+  "voluntarily-retired"       -> LicenseRating $ NegativeLicenseRating  (ScopedLicenseTag "OSI-keyword" kw NoLicenseTagText)
+  "superseded"                -> LicenseRating $ NegativeLicenseRating  (ScopedLicenseTag "OSI-keyword" kw NoLicenseTagText)
+  "non-reusable"              -> LicenseRating $ NegativeLicenseRating  (ScopedLicenseTag "OSI-keyword" kw NoLicenseTagText)
+  "special-purpose"           -> LicenseRating $ NeutralLicenseRating   (ScopedLicenseTag "OSI-keyword" kw NoLicenseTagText)
+  "other-miscellaneous"       -> LicenseRating $ NeutralLicenseRating   (ScopedLicenseTag "OSI-keyword" kw NoLicenseTagText)
+  "international"             -> LicenseRating $ NeutralLicenseRating   (ScopedLicenseTag "OSI-keyword" kw NoLicenseTagText)
+  "uncategorized"             -> LicenseRating $ NeutralLicenseRating   (ScopedLicenseTag "OSI-keyword" kw NoLicenseTagText)
+  _                           -> stmt (T.unpack kw)
 
 data OSI = OSI
 
 instance HasOriginalData OSI where
   getOriginalData OSI =
     FromUrl "https://opensource.org/licenses/" $
-      FromUrl "https://api.opensource.org/licenses/" $
-        FromUrl
-          "https://github.com/OpenSourceOrg/licenses"
-          NoPreservedOriginalData
+      FromUrl "https://opensource.org/api/license"
+        NoPreservedOriginalData
 
 instance Source OSI where
   getSource _ = Source "OSI"
   guardSource _ = lift $ do
-    request <- parseRequest "https://api.opensource.org"
-    result <- try (httpLBS request) :: IO (Either SomeException (Response ByteString))
+    request <- parseRequest "https://opensource.org/api/license"
+    result  <- try (httpLBS request) :: IO (Either SomeException (Response BL.ByteString))
     case result of
-        Left err -> do
-            errorM rootLoggerName "Network is not accessible"
-            return False
-        Right _ -> return True
+      Left _  -> do
+        errorM rootLoggerName "OSI API is not accessible"
+        return False
+      Right r -> return (getResponseStatusCode r == 200)
   getFacts OSI = do
-    response <- runExceptT OSI.allLicenses
+    response <- runExceptT fetchLicenses
     case response of
       Left err -> do
         stderrLogIO $ "Error: " ++ err
         pure mempty
-      Right licenses -> (return . V.fromList . map (wrapFact . OSILicense)) licenses
+      Right licenses ->
+        (return . V.fromList . map (wrapFact . OSILicense)) licenses
+
+fetchLicenses :: ExceptT String IO [OSILicenseV2]
+fetchLicenses = do
+  request  <- lift $ parseRequest "https://opensource.org/api/license"
+  result   <- lift $ (try (httpLBS request) :: IO (Either SomeException (Response BL.ByteString)))
+  response <- either (throwError . show) return result
+  let body = getResponseBody response
+  either throwError return (eitherDecode body)
